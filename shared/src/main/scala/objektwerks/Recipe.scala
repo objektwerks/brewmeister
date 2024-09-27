6d90dd1d071f9ee927159c1b103f09b6088d0f53
objektwerks <objektwerks@runbox.com>
2024-09-27 17:03:45 -0400

    added result

--- shared/src/main/scala/objektwerks/Recipe.scala
+++ shared/src/main/scala/objektwerks/Recipe.scala
@@ -12,6 +12,7 @@ final case class Water(id: Long)
 
 final case class Yeast(id: Long)
 
+final case class Result(id: Long)
 
 final case class Recipe(id: Long,
                         malts: List[Malt],
