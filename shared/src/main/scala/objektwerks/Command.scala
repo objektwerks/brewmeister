a7643736fc3381202a5e8256122e0285f3c5ce87
objektwerks <objektwerks@runbox.com>
2024-09-29 09:55:31 -0400

    enhanced command

--- shared/src/main/scala/objektwerks/Command.scala
+++ shared/src/main/scala/objektwerks/Command.scala
@@ -6,4 +6,6 @@ case object Sanitize extends Command
 
 final case class Prepare(recipe: Recipe)
 
-// Malt, Mill, Mash, Lauter, Sparge, Boil, Cool, Whirlpool, Ferment, Condition, Package
+final case class Malt(recipe: Recipe)
+
+//Mill, Mash, Lauter, Sparge, Boil, Cool, Whirlpool, Ferment, Condition, Package
