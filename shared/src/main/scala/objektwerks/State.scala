301269e1f945d83de1b34058d4ce39792f67dea1
objektwerks <objektwerks@runbox.com>
2024-09-29 10:27:21 -0400

    enhanced state

--- shared/src/main/scala/objektwerks/State.scala
+++ shared/src/main/scala/objektwerks/State.scala
@@ -1,3 +1,7 @@
 package objektwerks
 
 trait State
+
+case object Sanitizing extends State
+
+// Preparing, Malting, Milling, Mashing, Lautering, Sparging, Boiling, Cooling, Wirlpooling, Fermenting, Conditioning, Packaging
