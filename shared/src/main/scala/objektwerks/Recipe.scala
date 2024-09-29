1971acb02cf550b5a19d0bee805fffc0f5859024
objektwerks <objektwerks@runbox.com>
2024-09-29 10:33:03 -0400

    removed result from recipe

--- shared/src/main/scala/objektwerks/Recipe.scala
+++ shared/src/main/scala/objektwerks/Recipe.scala
@@ -12,13 +12,10 @@ final case class Water(id: Long)
 
 final case class Yeast(id: Long)
 
-final case class Result(id: Long)
-
 final case class Recipe(id: Long,
                         style: Style,
                         grains: List[Grain],
                         hops: List[Hop],
                         adjunts: List[Adjunct],
                         waters: List[Water],
-                        yeasts: List[Yeast],
-                        results: List[Result] = List.empty[Result])
+                        yeasts: List[Yeast])
