b1c968bc9ad7ac6a194eafb3cd28ade979d20faa
objektwerks <objektwerks@runbox.com>
2024-09-29 10:38:31 -0400

    implemented state

--- shared/src/main/scala/objektwerks/State.scala
+++ shared/src/main/scala/objektwerks/State.scala
@@ -4,4 +4,24 @@ trait State
 
 case object Sanitizing extends State
 
-// Preparing, Malting, Milling, Mashing, Lautering, Sparging, Boiling, Cooling, Wirlpooling, Fermenting, Conditioning, Packaging
+case object Preparing extends State
+
+case object Malting extends State
+
+case object Mashing extends State
+
+case object Lautering extends State
+
+case object Sparging extends State
+
+case object Boiling extends State
+
+case object Cooling extends State
+
+case object Whirlpooling extends State
+
+case object Fermenting extends State
+
+case object Conditioning extends State
+
+case object Packaging extends State
