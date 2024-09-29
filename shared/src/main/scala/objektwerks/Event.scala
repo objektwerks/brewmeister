e8501de0e01cdf94f8fc81401939a3d22fd397e2
objektwerks <objektwerks@runbox.com>
2024-09-29 10:35:42 -0400

    refactored event

--- shared/src/main/scala/objektwerks/Event.scala
+++ shared/src/main/scala/objektwerks/Event.scala
@@ -4,26 +4,26 @@ trait Event
 
 case object Sanitized extends Event
 
-final case class Prepared(recipe: Recipe) extends Event
+final case class Prepared(result: Result) extends Event
 
-final case class Malted(recipe: Recipe) extends Event
+final case class Malted(result: Result) extends Event
 
-final case class Milled(recipe: Recipe) extends Event
+final case class Milled(result: Result) extends Event
 
-final case class Mashed(recipe: Recipe) extends Event
+final case class Mashed(result: Result) extends Event
 
-final case class Lautered(recipe: Recipe) extends Event
+final case class Lautered(result: Result) extends Event
 
-final case class Sparged(recipe: Recipe) extends Event
+final case class Sparged(result: Result) extends Event
 
-final case class Boiled(recipe: Recipe) extends Event
+final case class Boiled(result: Result) extends Event
 
-final case class Cooled(recipe: Recipe) extends Event
+final case class Cooled(result: Result) extends Event
 
-final case class Whirlpooled(recipe: Recipe) extends Event
+final case class Whirlpooled(result: Result) extends Event
 
-final case class Fermented(recipe: Recipe) extends Event
+final case class Fermented(result: Result) extends Event
 
-final case class Conditioned(recipe: Recipe) extends Event
+final case class Conditioned(result: Result) extends Event
 
-final case class Packaged(recipe: Recipe) extends Event
+final case class Packaged(result: Result) extends Event
