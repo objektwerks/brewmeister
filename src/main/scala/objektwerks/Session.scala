4d6db74a2924bb73eecfe9d697702c23c70f3042
objektwerks <objektwerks@runbox.com>
2024-09-30 16:42:52 -0400

    edited session

--- src/main/scala/objektwerks/Session.scala
+++ src/main/scala/objektwerks/Session.scala
@@ -5,7 +5,7 @@ final case class Session(id: Long,
                         originalGravity: Double,
                         finalGravity: Double,
                         color: Int, // 1 - 40
-                        hopBitterness: Int, // 5 - 120
+                        hopBitterness: Int, // 0 - 120
                         alcoholByVolume: Double,
                         alcoholByWeight: Double,
                         calories: Double,
