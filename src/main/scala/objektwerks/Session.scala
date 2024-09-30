f2644debea39166b9b5faec8c4aeb890b1652ff4
objektwerks <objektwerks@runbox.com>
2024-09-30 17:00:32 -0400

    edited readme

--- src/main/scala/objektwerks/Session.scala
+++ src/main/scala/objektwerks/Session.scala
@@ -2,11 +2,11 @@ package objektwerks
 
 /* 
 Plato	SG (Specific Gravity)
-10	1.000
-12	1.048
-14	1.056
-16	1.064
-18	1.072
+10	  1.000
+12	  1.048
+14	  1.056
+16	  1.064
+18	  1.072
 */
 final case class Session(id: Long,
                          recipeId: Long,
