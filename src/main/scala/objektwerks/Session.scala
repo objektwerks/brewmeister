7a633e785d70907de64b3c28578e32239a7abb4a
objektwerks <objektwerks@runbox.com>
2024-09-30 14:15:12 -0400

    enhanced session

--- src/main/scala/objektwerks/Session.scala
+++ src/main/scala/objektwerks/Session.scala
@@ -1,4 +1,5 @@
 package objektwerks
 
 final case class Session(id: Long,
-                        recipeId: Long)
+                        recipeId: Long,
+                        originalGravity: Double)
