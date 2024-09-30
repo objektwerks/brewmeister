8787b5d380a3be65d98d289ae072ff57259937bb
objektwerks <objektwerks@runbox.com>
2024-09-30 10:39:22 -0400

    added app

---
+++ src/main/scala/objektwerks/App.scala
@@ -0,0 +1,14 @@
+package objektwerks
+
+import scalafx.application.JFXApp3
+// import scalafx.scene.image.Image
+
+object App extends JFXApp3:
+  override def start(): Unit =
+    stage = new JFXApp3.PrimaryStage:
+      // scene = View.scene
+      title = "Ciphers"
+      // minWidth = View.width
+      // minHeight = View.height
+      // icons.add( new Image( Image.getClass.getResourceAsStream("/cipher.png") ) )
+    stage.show()
