35fea67f0443278613a5021c179dfb067fd5af09
objektwerks <objektwerks@runbox.com>
2024-09-30 16:38:05 -0400

    edited session

--- src/main/scala/objektwerks/Session.scala
+++ src/main/scala/objektwerks/Session.scala
@@ -4,8 +4,8 @@ final case class Session(id: Long,
                         recipeId: Long,
                         originalGravity: Double,
                         finalGravity: Double,
-                        color: Int,
-                        hopBitterness: Int,
+                        color: Int, // 1 - 40
+                        hopBitterness: Int, // 5 - 120
                         alcoholByVolume: Double,
                         alcoholByWeight: Double,
                         calories: Double,
