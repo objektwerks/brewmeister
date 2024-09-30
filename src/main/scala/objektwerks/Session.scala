14866107b31108a84834ba46d6fc401728eaaca2
objektwerks <objektwerks@runbox.com>
2024-09-30 16:47:58 -0400

    edited session

--- src/main/scala/objektwerks/Session.scala
+++ src/main/scala/objektwerks/Session.scala
@@ -9,13 +9,13 @@ Plato	SG (Specific Gravity)
 18	1.072
 */
 final case class Session(id: Long,
-                        recipeId: Long,
-                        originalGravity: Double, // 1.000 - 1.072
-                        finalGravity: Double, // 1.000 - 1.072
-                        color: Int, // 1 - 40
-                        hopBitterness: Int, // 0 - 120
-                        alcoholByVolume: Double,
-                        alcoholByWeight: Double,
-                        calories: Double,
-                        mashEfficiency: Double,
-                        brewhouseEfficiency: Double)
+                         recipeId: Long,
+                         originalGravity: Double, // 1.000 - 1.072
+                         finalGravity: Double, // 1.000 - 1.072
+                         color: Int, // 1 - 40
+                         hopBitterness: Int, // 0 - 120
+                         alcoholByVolume: Double,
+                         alcoholByWeight: Double,
+                         calories: Double,
+                         mashEfficiency: Double,
+                         brewhouseEfficiency: Double)
