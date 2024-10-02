d85982dcf28fc193fd66ce397484e14f86481b19
objektwerks <objektwerks@runbox.com>
2024-10-02 00:53:17 -0400

    removed events from listener

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -4,5 +4,3 @@ final class Listener:
   def onCommand(command: Command): Unit = scribe.info(s"*** command: $command")
 
   def onEvent(event: Event): Unit = scribe.info(s"*** event: $event")
-
-  def onEvents(events: Event*): Unit = events.foreach( event => scribe.info(s"*** event: $event") )
