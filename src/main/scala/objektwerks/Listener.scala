cbc9f0c6a7fc3f06dbb8417daa8ce8d17b37186a
objektwerks <objektwerks@runbox.com>
2024-10-02 00:20:08 -0400

    refactored listener

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -1,6 +1,8 @@
 package objektwerks
 
 final class Listener:
-  def onEvents(events: Event*): Unit = events.foreach( event => scribe.info(s"*** event: $event") )
-
   def onCommand(command: Command): Unit = scribe.info(s"*** command: $command")
+
+  def onEvent(event: Event): Unit = scribe.info(s"*** event: $event")
+
+  def onEvents(events: Event*): Unit = events.foreach( event => scribe.info(s"*** event: $event") )
