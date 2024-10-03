eecf416f37ab0217a8ef8dc70c84a2b8ce641ade
objektwerks <objektwerks@runbox.com>
2024-10-03 14:22:08 -0400

    added keg

--- src/main/scala/objektwerks/Container.scala
+++ src/main/scala/objektwerks/Container.scala
@@ -9,3 +9,5 @@ final case class BoilKettle() extends Container
 final case class FementationKettle() extends Container
 
 final case class Bottle() extends Container
+
+final case class Keg() extends Container
