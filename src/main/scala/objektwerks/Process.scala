dbd2594571ad0c1cb89d24b229980783b1614395
objektwerks <objektwerks@runbox.com>
2024-10-03 16:33:52 -0400

    moved container and unit to process

--- src/main/scala/objektwerks/Process.scala
+++ src/main/scala/objektwerks/Process.scala
@@ -29,3 +29,20 @@ final case class Fermenting(fermentationKettle: FementationKettle) extends Step
 final case class Conditioning(fermentationKettle: FementationKettle) extends Step
 
 final case class Packaging(container: Bottle | Keg) extends Step
+
+enum Unit:
+  case oz, gl, l
+
+sealed trait Container:
+  def volume: Double
+  def unit: Unit
+
+final case class MashTun(volume: Double, unit: Unit) extends Container
+
+final case class BoilKettle(volume: Double, unit: Unit) extends Container
+
+final case class FementationKettle(volume: Double, unit: Unit) extends Container
+
+final case class Bottle(volume: Double, unit: Unit) extends Container
+
+final case class Keg(volume: Double, unit: Unit) extends Container
