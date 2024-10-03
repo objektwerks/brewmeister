ce57786d185c2474c4315e55285de280cfd01c06
objektwerks <objektwerks@runbox.com>
2024-10-03 10:52:55 -0400

    removed listener

--- src/main/scala/objektwerks/Listener.scala
+++
@@ -1,6 +0,0 @@
-package objektwerks
-
-final class Listener:
-  def onCommand(command: Command): Unit = scribe.info(s"*** command: $command")
-
-  def onEvent(event: Event): Unit = scribe.info(s"*** event: $event")
