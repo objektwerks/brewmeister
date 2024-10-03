1493457cbcfb049e2adc917e8a5d79eb2eaf511d
objektwerks <objektwerks@runbox.com>
2024-10-03 10:56:59 -0400

    refactored brewer

--- src/main/scala/objektwerks/Brewer.scala
+++ src/main/scala/objektwerks/Brewer.scala
@@ -6,31 +6,18 @@ import ox.supervised
 final class Brewer:
   def brew(recipe: Recipe): List[Event] =
     supervised:
-      val sanitized = Actor.create( Sanitizer() ).ask( _.sanitize( Sanitize() ) )
-      val prepared = Actor.create( Preparer() ).ask( _.prepare( Prepare(recipe) ) )
-      val malted = Actor.create( Malter() ).ask( _.malt( Malt(recipe) ) )
-      val milled = Actor.create( Miller() ).ask( _.mill( Mill(recipe) ) )
-      val mashed = Actor.create( Masher() ).ask( _.mash( Mash(recipe) ) )
-      val lautered = Actor.create( Lauterer() ).ask( _.lauter( Lauter() ) )
-      val sparged = Actor.create( Sparger() ).ask( _.sparge( Sparge() ) )
-      val boiled = Actor.create( Boiler() ).ask( _.boil( Boil(recipe) ) )
-      val cooled = Actor.create( Cooler() ).ask( _.cool( Cool() ) )
-      val whirlpooled = Actor.create( Whirlpooler() ).ask( _.whirlpool( Whirlpool(recipe) ) )
-      val fermented = Actor.create( Fermenter() ).ask( _.ferment( Ferment(recipe) ) )
-      val conditioned = Actor.create( Conditioner() ).ask( _.condition( Condition(recipe) ) )
-      val packaged = Actor.create( Packager() ).ask( _.`package`( Package() ) )
       List(
-        sanitized,
-        prepared,
-        malted,
-        milled,
-        mashed,
-        lautered,
-        sparged,
-        boiled,
-        cooled,
-        whirlpooled,
-        fermented,
-        conditioned,
-        packaged
+        Actor.create( Sanitizer() ).ask( _.sanitize( Sanitize() ) ),
+        Actor.create( Preparer() ).ask( _.prepare( Prepare(recipe) ) ),
+        Actor.create( Malter() ).ask( _.malt( Malt(recipe) ) ),
+        Actor.create( Miller() ).ask( _.mill( Mill(recipe) ) ),
+        Actor.create( Masher() ).ask( _.mash( Mash(recipe) ) ),
+        Actor.create( Lauterer() ).ask( _.lauter( Lauter() ) ),
+        Actor.create( Sparger() ).ask( _.sparge( Sparge() ) ),
+        Actor.create( Boiler() ).ask( _.boil( Boil(recipe) ) ),
+        Actor.create( Cooler() ).ask( _.cool( Cool() ) ),
+        Actor.create( Whirlpooler() ).ask( _.whirlpool( Whirlpool(recipe) ) ),
+        Actor.create( Fermenter() ).ask( _.ferment( Ferment(recipe) ) ),
+        Actor.create( Conditioner() ).ask( _.condition( Condition(recipe) ) ),
+        Actor.create( Packager() ).ask( _.`package`( Package() ) )
       )
