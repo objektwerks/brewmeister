33593d3922b31b1a734db8260573f3f0755233b9
objektwerks <objektwerks@runbox.com>
2024-10-03 10:28:58 -0400

    extracted brewer from actors

--- src/main/scala/objektwerks/Actor.scala
+++ src/main/scala/objektwerks/Actor.scala
@@ -1,27 +1,7 @@
 package objektwerks
 
-import ox.channels.Actor
-import ox.supervised
-
 sealed trait Actor:
   def close: Unit = scribe.info(s"*** actor closing ...")
-
-final class Brewer extends Actor:
-  def brew(recipe: Recipe, listener: Listener): Unit =
-    supervised:
-      Actor.create( Sanitizer() ).ask( _.sanitize( Sanitize(listener) ) )
-      Actor.create( Preparer() ).ask( _.prepare( Prepare(recipe), listener ) )
-      Actor.create( Malter() ).ask( _.malt( Malt(recipe), listener ) )
-      Actor.create( Miller() ).ask( _.mill( Mill(recipe), listener ) )
-      Actor.create( Masher() ).ask( _.mash( Mash(recipe), listener ) )
-      Actor.create( Lauterer() ).ask( _.mash( Lauter(), listener ) )
-      Actor.create( Sparger() ).ask( _.mash( Sparge(), listener ) )
-      Actor.create( Boiler() ).ask( _.mash( Boil(recipe), listener ) )
-      Actor.create( Cooler() ).ask( _.mash( Cool(), listener ) )
-      Actor.create( Whirlpooler() ).ask( _.mash( Whirlpool(recipe), listener ) )
-      Actor.create( Fermenter() ).ask( _.mash( Ferment(recipe), listener ) )
-      Actor.create( Conditioner() ).ask( _.mash( Condition(), listener ) )
-      Actor.create( Packager() ).ask( _.mash( Package(), listener ) )
 
 final class Sanitizer extends Actor:
   def sanitize(sanitize: Sanitize): Unit =
