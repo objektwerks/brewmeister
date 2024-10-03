0a6a29b2cb0f38acdb8aacda90ab2efaacf54e4d
objektwerks <objektwerks@runbox.com>
2024-10-03 08:29:39 -0400

    edited adjunct

--- src/main/scala/objektwerks/Recipe.scala
+++ src/main/scala/objektwerks/Recipe.scala
@@ -7,7 +7,7 @@ final case class Grain(typeof: String, step: Step)
 
 final case class Hop(typeof: String, amount: Int, step: Step)
 
-final case class Adjunct(typeof: String, oz: Int, step: Step)
+final case class Adjunct(typeof: String, amount: Int, step: Step)
 
 final case class Yeast(typeof: String, step: Step)
 
