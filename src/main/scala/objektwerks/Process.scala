f9f476d3ffa4ed3b675b320d32134632530170aa
objektwerks <objektwerks@runbox.com>
2024-10-03 16:39:11 -0400

    refactored container

--- src/main/scala/objektwerks/Process.scala
+++ src/main/scala/objektwerks/Process.scala
@@ -12,37 +12,31 @@ final case class Malting() extends Step
 
 final case class Milling() extends Step
 
-final case class Mashing(mashTun: MashTun) extends Step
+final case class Mashing(mashTun: Container) extends Step
 
-final case class Lautering(mashTun: MashTun) extends Step
+final case class Lautering(mashTun: Container) extends Step
 
-final case class Sparging(mashTun: MashTun) extends Step
+final case class Sparging(mashTun: Container) extends Step
 
-final case class Boiling(boilKettle: BoilKettle) extends Step
+final case class Boiling(boilKettle: Container) extends Step
 
-final case class Cooling(boilKettle: BoilKettle) extends Step
+final case class Cooling(boilKettle: Container) extends Step
 
-final case class Whirlpooling(boilKettle: BoilKettle) extends Step
+final case class Whirlpooling(boilKettle: Container) extends Step
 
-final case class Fermenting(fermentationKettle: FementationKettle) extends Step
+final case class Fermenting(fermentationKettle: Container) extends Step
 
-final case class Conditioning(fermentationKettle: FementationKettle) extends Step
+final case class Conditioning(fermentationKettle: Container) extends Step
 
-final case class Packaging(container: Bottle | Keg) extends Step
+final case class Packaging(container: Container) extends Step
+
+enum ContainerType:
+  case mashTun, boilKettle, fermentationKettle, bottle, keg
 
 enum Unit:
   case oz, gl, l
 
-sealed trait Container:
-  def volume: Double
-  def unit: Unit
-
-final case class MashTun(volume: Double, unit: Unit) extends Container
-
-final case class BoilKettle(volume: Double, unit: Unit) extends Container
-
-final case class FementationKettle(volume: Double, unit: Unit) extends Container
-
-final case class Bottle(volume: Double, unit: Unit) extends Container
-
-final case class Keg(volume: Double, unit: Unit) extends Container
+final case class Container(id: Long,
+                           container: ContainerType,
+                           volume: Double,
+                           unit: Unit)
