92b3fb48111471b93124c87d18cb334ddcdbf768
objektwerks <objektwerks@runbox.com>
2024-10-03 16:56:34 -0400

    added id and process id to steps

--- src/main/scala/objektwerks/Process.scala
+++ src/main/scala/objektwerks/Process.scala
@@ -4,31 +4,31 @@ final case class Process(id: Long, steps: List[Step])
 
 sealed trait Step
 
-final case class Sanitizing() extends Step
+final case class Sanitizing(id: Long, processId: Long) extends Step
 
-final case class Preparing() extends Step
+final case class Preparing(id: Long, processId: Long) extends Step
 
-final case class Malting() extends Step
+final case class Malting(id: Long, processId: Long) extends Step
 
-final case class Milling() extends Step
+final case class Milling(id: Long, processId: Long) extends Step
 
-final case class Mashing(mashTun: Container) extends Step
+final case class Mashing(id: Long, processId: Long, mashTun: Container) extends Step
 
-final case class Lautering(mashTun: Container) extends Step
+final case class Lautering(id: Long, processId: Long, mashTun: Container) extends Step
 
-final case class Sparging(mashTun: Container) extends Step
+final case class Sparging(id: Long, processId: Long, mashTun: Container) extends Step
 
-final case class Boiling(boilKettle: Container) extends Step
+final case class Boiling(id: Long, processId: Long, boilKettle: Container) extends Step
 
-final case class Cooling(boilKettle: Container) extends Step
+final case class Cooling(id: Long, processId: Long, boilKettle: Container) extends Step
 
-final case class Whirlpooling(boilKettle: Container) extends Step
+final case class Whirlpooling(id: Long, processId: Long, boilKettle: Container) extends Step
 
-final case class Fermenting(fermentationKettle: Container) extends Step
+final case class Fermenting(id: Long, processId: Long, fermentationKettle: Container) extends Step
 
-final case class Conditioning(fermentationKettle: Container) extends Step
+final case class Conditioning(id: Long, processId: Long, fermentationKettle: Container) extends Step
 
-final case class Packaging(container: Container) extends Step
+final case class Packaging(id: Long, processId: Long, bottleOrKeg: Container) extends Step
 
 enum ContainerType:
   case mashTun, boilKettle, fermentationKettle, bottle, keg
