d87b1672c7be2205e8ac8b5c757acea0edfae8df
objektwerks <objektwerks@runbox.com>
2024-10-04 09:39:46 -0400

    added metrics json

--- src/main/scala/objektwerks/Serializer.scala
+++ src/main/scala/objektwerks/Serializer.scala
@@ -4,4 +4,5 @@ import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
 object Serializer:
-  
+  given JsonValueCodec[Recipe] = JsonCodecMaker.make[Recipe]
+  given JsonValueCodec[Metrics] = JsonCodecMaker.make[Metrics]
