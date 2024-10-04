537f8f6fa6f2a411e786cc489e5105e9d3054346
objektwerks <objektwerks@runbox.com>
2024-10-04 09:41:16 -0400

    enhanced serializer

--- src/main/scala/objektwerks/Serializer.scala
+++ src/main/scala/objektwerks/Serializer.scala
@@ -6,3 +6,9 @@ import com.github.plokhotnyuk.jsoniter_scala.macros.*
 object Serializer:
   given JsonValueCodec[Recipe] = JsonCodecMaker.make[Recipe]
   given JsonValueCodec[Metrics] = JsonCodecMaker.make[Metrics]
+
+  given JsonValueCodec[Grain] = JsonCodecMaker.make[Grain]
+  given JsonValueCodec[Hop] = JsonCodecMaker.make[Hop]
+  given JsonValueCodec[Adjunct] = JsonCodecMaker.make[Adjunct]
+  given JsonValueCodec[Yeast] = JsonCodecMaker.make[Yeast]
+
