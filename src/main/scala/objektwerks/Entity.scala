b969d48ff42f9aef1e5b0960de70782f48c816dd
objektwerks <objektwerks@runbox.com>
2024-10-05 08:07:30 -0400

    refactored steps

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -18,19 +18,22 @@ final case class Process(started: String = now(),
 
 object Steps:
   def default: List[Step] =
+    val mashTun = MashTun(5.0, UoM.gl)
+    val boilKettle = BoilKettle(5.0, UoM.gl)
+    val fermentationKettle = FermentationKettle(5.0, UoM.gl)
     List(
       Sanitizing(),
       Preparing(),
       Malting(),
       Milling(),
-      Mashing(),
-      Lautering(),
-      Sparging(),
-      Boiling(),
-      Cooling(),
-      Whirlpooling(),
-      Fermenting(),
-      Conditioning(),
+      Mashing(mashTun = mashTun),
+      Lautering(mashTun = mashTun),
+      Sparging(mashTun = mashTun),
+      Boiling(boilKettle = boilKettle),
+      Cooling(boilKettle = boilKettle),
+      Whirlpooling(boilKettle = boilKettle),
+      Fermenting(fermentationKettle = fermentationKettle),
+      Conditioning(fermentationKettle = fermentationKettle),
       Packaging()
     )
 
@@ -43,15 +46,15 @@ final case class Sanitizing(step: Int = 1) extends Step
 final case class Preparing(step: Int = 2) extends Step
 final case class Malting(step: Int = 3) extends Step
 final case class Milling(step: Int = 4) extends Step
-final case class Mashing(step: Int = 5, mashTun: Container = MashTun(5.0, UoM.gl)) extends Step
-final case class Lautering(step: Int = 6, mashTun: Container = MashTun(5.0, UoM.gl)) extends Step
-final case class Sparging(step: Int = 7, mashTun: Container = MashTun(5.0, UoM.gl)) extends Step
-final case class Boiling(step: Int = 8, boilKettle: Container = BoilKettle(5.0, UoM.gl)) extends Step
-final case class Cooling(step: Int = 9, boilKettle: Container = BoilKettle(5.0, UoM.gl)) extends Step
-final case class Whirlpooling(step: Int = 10, boilKettle: Container = BoilKettle(5.0, UoM.gl)) extends Step
-final case class Fermenting(step: Int = 11, fermentationKettle: Container = FermentationKettle(5.0, UoM.gl)) extends Step
-final case class Conditioning(step: Int = 12, fermentationKettle: Container = FermentationKettle(5.0, UoM.gl)) extends Step
-final case class Packaging(step: Int = 13, bottleOrKeg: Container = Keg(5.0, UoM.gl)) extends Step
+final case class Mashing(step: Int = 5, mashTun: MashTun) extends Step
+final case class Lautering(step: Int = 6, mashTun: MashTun) extends Step
+final case class Sparging(step: Int = 7, mashTun: MashTun) extends Step
+final case class Boiling(step: Int = 8, boilKettle: BoilKettle) extends Step
+final case class Cooling(step: Int = 9, boilKettle: BoilKettle) extends Step
+final case class Whirlpooling(step: Int = 10, boilKettle: BoilKettle) extends Step
+final case class Fermenting(step: Int = 11, fermentationKettle: FermentationKettle) extends Step
+final case class Conditioning(step: Int = 12, fermentationKettle: FermentationKettle) extends Step
+final case class Packaging(step: Int = 13, keg: Keg = Keg(5.0, UoM.gl)) extends Step
 
 sealed trait Container derives ReadWriter:
   def volume: Double
@@ -60,7 +63,6 @@ sealed trait Container derives ReadWriter:
 final case class MashTun(volume: Double, unit: UoM) extends Container
 final case class BoilKettle(volume: Double, unit: UoM) extends Container
 final case class FermentationKettle(volume: Double, unit: UoM) extends Container
-final case class Bottle(volume: Double, unit: UoM) extends Container
 final case class Keg(volume: Double, unit: UoM) extends Container
 
 enum MixinStep derives ReadWriter:
