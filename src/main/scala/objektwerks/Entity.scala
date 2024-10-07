64626984bc70dc6f2dc72c8d13298fdd48ff6fab
objektwerks <objektwerks@runbox.com>
2024-10-07 06:45:05 -0400

    added format def to entity

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -7,6 +7,7 @@ import upickle.default.{ReadWriter => JsonSupport}
 def now(): String = LocalDateTime.now.toString
 def localDateTime(now: String): LocalDateTime = if now.nonEmpty then LocalDateTime.parse(now) else LocalDateTime.now
 
+def format(double: Double): Double = f"$double%1.1f".toDouble
 def formatGravity(double: Double): Double = f"$double%1.3f".toDouble
 
 enum UoM derives JsonSupport:
