9e158efb8784c8d1585ea8b4cdf51ea1501f5a45
objektwerks <objektwerks@runbox.com>
2024-10-11 17:45:39 -0400

    refactored listener

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable
 
 final class Listener:
   val listeners = mutable.ListBuffer.empty[Listener]
-  val logs = mutable.ListBuffer.empty[String]
+  val log = mutable.ListBuffer.empty[String]
 
   def register(listener: Listener): Unit =
     listeners += listener
@@ -12,14 +12,14 @@ final class Listener:
 
   def onCommand(command: Command): Unit =
     listeners.foreach( _.onCommand(command) )
-    scribe.info(command.toString)
+    log(command.toString)
 
   def onEvent(event: Event): Unit =
     listeners.foreach( _.onEvent(event) )
-    scribe.info(event.toString)
+    log(event.toString)
 
   def onMetrics(metrics: Metrics): Unit =
     listeners.foreach( _.onMetrics(metrics) )
-    scribe.info(metrics.toString)
+    log(metrics.toString)
 
-  def log(entry: String): Unit = logs += entry
+  def log(entry: String): Unit = log += entry
