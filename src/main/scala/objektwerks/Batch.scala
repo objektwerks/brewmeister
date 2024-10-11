bf17b0403bf2ff3ad8b4ee971733b886b6a401b8
objektwerks <objektwerks@runbox.com>
2024-10-11 14:25:00 -0400

    enhanced metrics abv

--- src/main/scala/objektwerks/Batch.scala
+++ src/main/scala/objektwerks/Batch.scala
@@ -137,7 +137,7 @@ object Metrics:
     val hopUtilization = (hopAlphaAcid * hopWeight) / hopVolume
     format( (hopWeight * hopAlphaAcid * hopUtilization) / 7.25 )
 
-  def alcoholByVolume(originalGravity: Double, finalGravity: Double): Double = (originalGravity - finalGravity) * 131
+  def alcoholByVolume(originalGravity: Double, finalGravity: Double): Double = format( (originalGravity - finalGravity) * 131 )
 
   def alcoholByWeight(alcoholByVolume: Double, finalGravity: Double): Double = (0.79 * alcoholByVolume) / finalGravity
 
