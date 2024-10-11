af0f1f64ecddcb13a4bce5b92768d22cb018cc59
objektwerks <objektwerks@runbox.com>
2024-10-11 12:50:50 -0400

    added register to listener

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -1,6 +1,8 @@
 package objektwerks
 
 final class Listener:
+  def register(listener: Listener): Unit = ???
+  
   def onCommand(command: Command): Unit = scribe.info(command.toString)
 
   def onEvent(event: Event): Unit = scribe.info(event.toString)
