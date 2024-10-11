148698c40c5d47e399e3c9b0f9dff54f6e603364
objektwerks <objektwerks@runbox.com>
2024-10-11 16:27:51 -0400

    refactored commands to replace recipe with batch

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -4,26 +4,26 @@ sealed trait Command
 
 final case class Sanitize(batchId: Int) extends Command
 
-final case class Prepare(batchId: Int, recipe: Recipe) extends Command
+final case class Prepare(batchId: Int, batch: Batch) extends Command
 
-final case class Malt(batchId: Int, recipe: Recipe) extends Command
+final case class Malt(batchId: Int, batch: Batch) extends Command
 
-final case class Mill(batchId: Int, recipe: Recipe) extends Command
+final case class Mill(batchId: Int, batch: Batch) extends Command
 
-final case class Mash(batchId: Int, recipe: Recipe) extends Command
+final case class Mash(batchId: Int, batch: Batch) extends Command
 
-final case class Lauter(batchId: Int, recipe: Recipe) extends Command
+final case class Lauter(batchId: Int, batch: Batch) extends Command
 
-final case class Sparge(batchId: Int, recipe: Recipe) extends Command
+final case class Sparge(batchId: Int, batch: Batch) extends Command
 
-final case class Boil(batchId: Int, recipe: Recipe) extends Command
+final case class Boil(batchId: Int, batch: Batch) extends Command
 
-final case class Cool(batchId: Int, recipe: Recipe) extends Command
+final case class Cool(batchId: Int, batch: Batch) extends Command
 
-final case class Whirlpool(batchId: Int, recipe: Recipe) extends Command
+final case class Whirlpool(batchId: Int, batch: Batch) extends Command
 
-final case class Ferment(batchId: Int, recipe: Recipe) extends Command
+final case class Ferment(batchId: Int, batch: Batch) extends Command
 
-final case class Condition(batchId: Int, recipe: Recipe) extends Command
+final case class Condition(batchId: Int, batch: Batch) extends Command
 
-final case class Package(batchId: Int, recipe: Recipe) extends Command
+final case class Package(batchId: Int, batch: Batch) extends Command
