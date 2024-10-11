22675a664ff980cd9a02487014f3c74a8f588329
objektwerks <objektwerks@runbox.com>
2024-10-11 18:01:31 -0400

    refactored listener

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -4,16 +4,11 @@ import scala.collection.mutable
 
 final class Listener:
   private val listeners = mutable.ListBuffer.empty[Listener]
-  val commands = mutable.ListBuffer.empty[Command]
   val events = mutable.ListBuffer.empty[Event]
 
   def register(listener: Listener): Unit =
     listeners += listener
     ()
-
-  def onCommand(command: Command): Unit =
-    commands += command
-    listeners.foreach( _.onCommand(command) )
 
   def onEvent(event: Event): Unit =
     events += event
