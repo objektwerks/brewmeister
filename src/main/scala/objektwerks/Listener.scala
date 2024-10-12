4f72c01d138201dae462c3f8cb971b5af7b494d6
objektwerks <objektwerks@runbox.com>
2024-10-12 09:34:43 -0400

    refactoreed listener metrics def

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -6,30 +6,7 @@ final class Listener(batchId: Int):
   private val listeners = mutable.ListBuffer.empty[Listener]
   val events = mutable.ListBuffer.empty[Event]
 
-  def metrics: Metrics =
-    var metrics = Metrics(batchId = batchId)
-    events.foreach { event =>
-      event match
-        case Mashed(_, pH) =>
-          metrics = metrics.copy(pH = pH)
-        case Sparged(_, mashEfficiency) =>
-          metrics = metrics.copy(mashEfficiency = mashEfficiency)
-        case Whirlpooled(_, originalGravity) =>
-          metrics = metrics.copy(originalGravity = originalGravity)
-        case Fermented(_, finalGravity) =>
-          metrics = metrics.copy(finalGravity = finalGravity)
-        case Conditioned(_, srmColor) =>
-          metrics = metrics.copy(srmColor = srmColor)
-        case Packaged(_, ibuBitterness, alcoholByVolume, alcoholByWeight, calories, brewhouseEfficiency) =>
-          metrics = metrics.copy(ibuBitterness = ibuBitterness,
-                                alcoholByVolume = alcoholByVolume,
-                                alcoholByWeight = alcoholByWeight,
-                                calories = calories,
-                                brewhouseEfficiency = brewhouseEfficiency)
-        case _ =>
-    }
-
-    metrics
+  def metrics: Metrics = Metrics.build(batchId, events.toList)
 
   def register(listener: Listener): Unit =
     listeners += listener
