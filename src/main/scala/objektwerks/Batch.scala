d679c9b8eb202d83dc369b5952eca93c6ded9770
objektwerks <objektwerks@runbox.com>
2024-10-12 11:10:17 -0400

    extract common defs from batch and put into common

--- src/main/scala/objektwerks/Batch.scala
+++ src/main/scala/objektwerks/Batch.scala
@@ -1,20 +1,6 @@
 package objektwerks
 
-import java.time.LocalDateTime
-
 import upickle.default.{ReadWriter => JsonSupport}
-
-def now(): String = LocalDateTime.now.toString
-def localDateTime(now: String): LocalDateTime = if now.nonEmpty then LocalDateTime.parse(now) else LocalDateTime.now
-
-def format(double: Double): Double = f"$double%1.1f".toDouble
-def formatGravity(double: Double): Double = f"$double%1.3f".toDouble
-
-enum UoM derives JsonSupport:
-  case oz, gl, ml, l, lb, kg
-
-enum UoT derives JsonSupport:
-  case minutes, days, weeks, months
 
 object Batch:
   def default: Batch = Batch(recipe = Recipe.default)
