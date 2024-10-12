329b4e726ce39263bab33b04f260dde7d30b20c2
objektwerks <objektwerks@runbox.com>
2024-10-12 09:33:53 -0400

    added build metrics to metrics

--- src/main/scala/objektwerks/Metrics.scala
+++ src/main/scala/objektwerks/Metrics.scala
@@ -20,6 +20,31 @@ object Metrics:
             mashEfficiency = 70,
             brewhouseEfficiency = 71)
 
+  def build(batchId: Int,
+            events: List[Event]): Metrics =
+    var metrics = Metrics(batchId = batchId)
+    events.foreach { event =>
+      event match
+        case Mashed(_, pH) =>
+          metrics = metrics.copy(pH = pH)
+        case Sparged(_, mashEfficiency) =>
+          metrics = metrics.copy(mashEfficiency = mashEfficiency)
+        case Whirlpooled(_, originalGravity) =>
+          metrics = metrics.copy(originalGravity = originalGravity)
+        case Fermented(_, finalGravity) =>
+          metrics = metrics.copy(finalGravity = finalGravity)
+        case Conditioned(_, srmColor) =>
+          metrics = metrics.copy(srmColor = srmColor)
+        case Packaged(_, ibuBitterness, alcoholByVolume, alcoholByWeight, calories, brewhouseEfficiency) =>
+          metrics = metrics.copy(ibuBitterness = ibuBitterness,
+                                alcoholByVolume = alcoholByVolume,
+                                alcoholByWeight = alcoholByWeight,
+                                calories = calories,
+                                brewhouseEfficiency = brewhouseEfficiency)
+        case _ =>
+    }
+    metrics
+
   def srmColor(grainWeight: Double,
                grainColor: Double,
                batchVolume: Double): Int =
