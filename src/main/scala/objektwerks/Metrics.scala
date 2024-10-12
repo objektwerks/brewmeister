bc1298be3f930037e24bf0e770903590eb7e364c
objektwerks <objektwerks@runbox.com>
2024-10-12 11:04:20 -0400

    added mash efficiency for grains in metrics

--- src/main/scala/objektwerks/Metrics.scala
+++ src/main/scala/objektwerks/Metrics.scala
@@ -78,6 +78,9 @@ object Metrics:
     val carbohydrateCalories = (finalGravity * 13) * packageVolume
     ( alcoholCalories + carbohydrateCalories ).toInt
 
+  def mashEfficiency(grains: List[Grain]): Double =
+    grains.map { grain => grain.potentialMashExtract }.sum
+
   def mashEfficiency(actualMashExtract: Double,
                      potentialMashExtract: Double): Int =
     ( (actualMashExtract / potentialMashExtract) * 100 ).toInt
