6d5c2e449f928da87bae32572e8d9e9b721d0a12
objektwerks <objektwerks@runbox.com>
2024-10-12 08:53:04 -0400

    removed batch id from events

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -2,32 +2,31 @@ package objektwerks
 
 sealed trait Event
 
-final case class Sanitized(batchId: Int, log: List[String]) extends Event
+final case class Sanitized(log: List[String]) extends Event
 
-final case class Prepared(batchId: Int, log: List[String]) extends Event
+final case class Prepared(log: List[String]) extends Event
 
-final case class Malted(batchId: Int, log: List[String]) extends Event
+final case class Malted(log: List[String]) extends Event
 
-final case class Milled(batchId: Int, log: List[String]) extends Event
+final case class Milled(log: List[String]) extends Event
 
-final case class Mashed(batchId: Int, log: List[String], pH: Double) extends Event
+final case class Mashed(log: List[String], pH: Double) extends Event
 
-final case class Lautered(batchId: Int, log: List[String]) extends Event
+final case class Lautered(log: List[String]) extends Event
 
-final case class Sparged(batchId: Int, log: List[String], mashEfficiency: Int) extends Event
+final case class Sparged(log: List[String], mashEfficiency: Int) extends Event
 
-final case class Boiled(batchId: Int, log: List[String]) extends Event
+final case class Boiled(log: List[String]) extends Event
 
-final case class Cooled(batchId: Int, log: List[String]) extends Event
+final case class Cooled(log: List[String]) extends Event
 
-final case class Whirlpooled(batchId: Int, log: List[String], originalGravity: Double) extends Event
+final case class Whirlpooled(log: List[String], originalGravity: Double) extends Event
 
-final case class Fermented(batchId: Int, log: List[String], finalGravity: Double) extends Event
+final case class Fermented(log: List[String], finalGravity: Double) extends Event
 
-final case class Conditioned(batchId: Int, log: List[String], srmColor: Int) extends Event
+final case class Conditioned(log: List[String], srmColor: Int) extends Event
 
-final case class Packaged(batchId: Int,
-                          log: List[String],
+final case class Packaged(log: List[String],
                           ibuBitterness: Int,
                           alcoholByVolume: Double,
                           alcoholByWeight: Double,
