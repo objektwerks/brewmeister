5d63ca9231fca1bc97ffd1f9874f9100a0ad884e
objektwerks <objektwerks@runbox.com>
2024-10-12 08:56:36 -0400

    reformatted events

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -10,21 +10,26 @@ final case class Malted(log: List[String]) extends Event
 
 final case class Milled(log: List[String]) extends Event
 
-final case class Mashed(log: List[String], pH: Double) extends Event
+final case class Mashed(log: List[String],
+                        pH: Double) extends Event
 
 final case class Lautered(log: List[String]) extends Event
 
-final case class Sparged(log: List[String], mashEfficiency: Int) extends Event
+final case class Sparged(log: List[String],
+                         mashEfficiency: Int) extends Event
 
 final case class Boiled(log: List[String]) extends Event
 
 final case class Cooled(log: List[String]) extends Event
 
-final case class Whirlpooled(log: List[String], originalGravity: Double) extends Event
+final case class Whirlpooled(log: List[String],
+                             originalGravity: Double) extends Event
 
-final case class Fermented(log: List[String], finalGravity: Double) extends Event
+final case class Fermented(log: List[String],
+                           finalGravity: Double) extends Event
 
-final case class Conditioned(log: List[String], srmColor: Int) extends Event
+final case class Conditioned(log: List[String],
+                             srmColor: Int) extends Event
 
 final case class Packaged(log: List[String],
                           ibuBitterness: Int,
