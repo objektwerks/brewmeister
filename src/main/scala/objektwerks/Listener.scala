8d27c9cb1c423ffe9917b260fc1c5417cc419a97
objektwerks <objektwerks@runbox.com>
2024-10-12 08:32:10 -0400

    enhanced listener

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -6,6 +6,8 @@ final class Listener:
   private val listeners = mutable.ListBuffer.empty[Listener]
   val events = mutable.ListBuffer.empty[Event]
 
+  def metrics: Metrics = Metrics.default // TODO! Derive from events!
+
   def register(listener: Listener): Unit =
     listeners += listener
     ()
