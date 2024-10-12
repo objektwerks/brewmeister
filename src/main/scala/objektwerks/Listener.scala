737cce491d31bc8bd6604ef461e9bb3183e2c802
objektwerks <objektwerks@runbox.com>
2024-10-12 12:54:04 -0400

    moved metrics method to listener

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -2,7 +2,7 @@ package objektwerks
 
 import scala.collection.mutable
 
-final class Listener(batchId: Int):
+final class Listener:
   private val listeners = mutable.ListBuffer.empty[Listener]
   private val events = mutable.ListBuffer.empty[Event]
 
@@ -14,8 +14,6 @@ final class Listener(batchId: Int):
     events += event
     listeners.foreach( _.onEvent(event) )
 
-  def metrics: Metrics = Metrics.build(batchId, events.toList)
-
   def originalGravity: Double =
     events.toList match
       case e :: es if e.isInstanceOf[Whirlpooled] => e.asInstanceOf[Whirlpooled].originalGravity
@@ -25,3 +23,28 @@ final class Listener(batchId: Int):
     events.toList match
       case e :: es if e.isInstanceOf[Fermented] => e.asInstanceOf[Fermented].finalGravity
       case _ => 0.0
+
+  def metrics(batchId: Int,
+              events: List[Event]): Metrics =
+    var metrics = Metrics(batchId = batchId)
+    events.foreach { event =>
+      event match
+        case Mashed(_, pH) =>
+          metrics = metrics.copy(pH = pH)
+        case Sparged(_, mashEfficiency) =>
+          metrics = metrics.copy(mashEfficiency = mashEfficiency)
+        case Whirlpooled(_, originalGravity) =>
+          metrics = metrics.copy(originalGravity = originalGravity)
+        case Fermented(_, finalGravity) =>
+          metrics = metrics.copy(finalGravity = finalGravity)
+        case Conditioned(_, srmColor) =>
+          metrics = metrics.copy(srmColor = srmColor)
+        case Packaged(_, ibuBitterness, alcoholByVolume, alcoholByWeight, calories, brewhouseEfficiency) =>
+          metrics = metrics.copy(ibuBitterness = ibuBitterness,
+                                alcoholByVolume = alcoholByVolume,
+                                alcoholByWeight = alcoholByWeight,
+                                calories = calories,
+                                brewhouseEfficiency = brewhouseEfficiency)
+        case _ =>
+    }
+    metrics
