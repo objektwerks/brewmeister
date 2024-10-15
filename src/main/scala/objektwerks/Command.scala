ac1b0d356209f63d4e45d0498fd78d033d354700
objektwerks <objektwerks@runbox.com>
2024-10-15 18:09:23 -0400

    refactoring command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -4,49 +4,47 @@ sealed trait Command
 
 final case class Sanitize(recipe: Recipe) extends Command
 
-case object Sanitized extends Command
+case object Sanitizing extends Command
 
 final case class Prepare(recipe: Recipe) extends Command
 
-case object Prepared extends Command
+case object Preparing extends Command
 
 final case class Malt(recipe: Recipe) extends Command
 
-case object Malted extends Command
+case object Malting extends Command
 
 final case class Mill(recipe: Recipe) extends Command
 
-case object Milled extends Command
+case object Milling extends Command
 
 final case class Mash(recipe: Recipe, pH: Double) extends Command // remove pH
 
-final case class Mashed(pH: Double) extends Command
-
-case object Mashed extends Command
+case object Mashing extends Command
 
 final case class Lauter(recipe: Recipe) extends Command
 
-case object Lautered extends Command
+case object Lautering extends Command
 
 final case class Sparge(recipe: Recipe, actualMashExtract: Double) extends Command // removed actualMashExtract
 
-final case class Spared(actualMashExtract: Double) extends Command
+case object Sparing extends Command
 
 final case class Boil(recipe: Recipe) extends Command
 
-case object Boiled extends Command
+case object Boiling extends Command
 
 final case class Cool(recipe: Recipe) extends Command
 
-case object Cooled extends Command
+case object Cooling extends Command
 
 final case class Whirlpool(recipe: Recipe, originalGravity: Double) extends Command // remove originalGravity
 
-final case class Whirlpooled(originalGravity: Double) extends Command
+case object Whirlpooling extends Command
 
-final case class Ferment(recipe: Recipe, finalGravity: Double) extends Command
+final case class Ferment(recipe: Recipe, finalGravity: Double) extends Command // remove finalGravity
 
-final case class Fermented(finalGravity: Double) extends Command
+case object Fermented extends Command
 
 final case class Condition(recipe: Recipe) extends Command
 
@@ -54,4 +52,4 @@ case object Conditioned extends Command
 
 final case class Keg(recipe: Recipe, actualFermentableExtract: Double) extends Command
 
-final case class Kegged(actualFermentableExtract: Double) extends Command
+case object Kegged(actualFermentableExtract: Double) extends Command // remove actualFermentableExtract
