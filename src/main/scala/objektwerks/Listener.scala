eac3eaa1635aea7aab7a07429d2680a9205286d8
objektwerks <objektwerks@runbox.com>
2024-10-18 17:57:49 -0400

    enhanced listener batch

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -40,6 +40,8 @@ final class Listener:
           batch = batch.copy(log = batch.log ++ log)
         case PhLogged(pH) =>
           batch = batch.copy(pH = pH)
+        case Lautered(log) =>
+          batch = batch.copy(log = batch.log ++ log)
         case Sparged(log) =>
           batch = batch.copy(log = batch.log ++ log)
         case MashEfficiencyLogged(mashEfficiency) =>
