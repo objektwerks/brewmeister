eab9dd760e764e1ee16c46743f024113eeb150a5
objektwerks <objektwerks@runbox.com>
2024-10-22 15:20:51 -0400

    enhanced common local data time via string extension

--- src/main/scala/objektwerks/Common.scala
+++ src/main/scala/objektwerks/Common.scala
@@ -4,10 +4,6 @@ import java.time.LocalDateTime
 
 import upickle.default.{ReadWriter => JsonSupport}
 
-def now(): String = LocalDateTime.now.toString
-
-def localDateTime(now: String): LocalDateTime = if now.nonEmpty then LocalDateTime.parse(now) else LocalDateTime.now
-
 enum UoM derives JsonSupport:
   case oz, gl, ml, l, lb, kg
 
@@ -16,5 +12,9 @@ enum UoT derives JsonSupport:
 
 extension (double: Double)
   def format: Double = f"$double%1.1f".toDouble
-
   def formatGravity: Double = f"$double%1.3f".toDouble
+
+extension (now: String)
+  def localDateTime: LocalDateTime = if now.nonEmpty then LocalDateTime.parse(now) else LocalDateTime.now
+
+def now(): String = LocalDateTime.now.toString
