f6fa8558fcc3941bcffebad40c1e26e33a801b09
objektwerks <objektwerks@runbox.com>
2024-10-23 16:44:16 -0400

    added recipe test

---
+++ src/test/scala/objektwerks/RecipeTest.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class RecipeTest extends AnyFunSuite with Matchers:
+  test("recipe"):
+    true
