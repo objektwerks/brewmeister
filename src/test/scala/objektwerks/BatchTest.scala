027cb60f24aee3d78faea4fbc76f9a1de460b8ed
objektwerks <objektwerks@runbox.com>
2024-10-23 16:44:53 -0400

    added batch test

---
+++ src/test/scala/objektwerks/BatchTest.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class BatchTest extends AnyFunSuite with Matchers:
+  test("batch"):
+    true
