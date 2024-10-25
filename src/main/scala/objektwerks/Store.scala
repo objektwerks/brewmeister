0016df4075f122120dd22e0e478d3de61eeef907
objektwerks <objektwerks@runbox.com>
2024-10-25 14:32:48 -0400

    enhanced store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -5,6 +5,8 @@ final class Store(storePath: String):
 
   def listRecipes: List[Recipe] = ???
 
-  def addRecipe(recipe: Recipe) = ???
+  def addRecipe(recipe: Recipe): Unit = ???
 
   def listBatches: List[Batch] = ???
+
+  def addBatch(batch: Batch): Unit = ???
