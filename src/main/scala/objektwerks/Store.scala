8aea5ae012cec5173327ee96774d81351a09e933
objektwerks <objektwerks@runbox.com>
2024-10-27 18:12:38 -0400

    edited store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -14,21 +14,21 @@ final class Store():
   def listRecipes: IndexedSeq[Path] =
     os.list(recipesPath)
 
-  def readRecipe(name: String): Recipe =
-    val recipeAsJson = os.read(recipesPath / s"$name.json")
-    readJson[Recipe](recipeAsJson)
-
   def writeRecipe(recipe: Recipe): Unit =
     val recipeAsJson = writeJson(recipe)
     os.write.over(recipesPath / s"${recipe.created}.json", recipeAsJson)
 
+  def readRecipe(name: String): Recipe =
+    val recipeAsJson = os.read(recipesPath / s"$name.json")
+    readJson[Recipe](recipeAsJson)
+
   def listBatches: IndexedSeq[Path] =
     os.list(batchesPath)
-
-  def readBatch(name: String): Batch =
-    val batchAsJson = os.read(batchesPath / s"$name.json")
-    readJson[Batch](batchAsJson)
 
   def writeBatch(batch: Batch): Unit =
     val batchAsJson = writeJson(batch)
     os.write.over(batchesPath / s"${batch.created}.json", batchAsJson)
+
+  def readBatch(name: String): Batch =
+    val batchAsJson = os.read(batchesPath / s"$name.json")
+    readJson[Batch](batchAsJson)
