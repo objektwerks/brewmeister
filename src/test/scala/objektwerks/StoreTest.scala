9f973f0e2defd7ebe6357e915807f094a941a601
objektwerks <objektwerks@runbox.com>
2024-10-27 16:51:18 -0400

    enhanced store test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -1,8 +1,22 @@
 package objektwerks
 
+import com.typesafe.config.ConfigFactory
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import os.Path
+
 final class StoreTest extends AnyFunSuite with Matchers:
+  val conf = ConfigFactory.load("/test.conf")
+  val pathRecipes = Path( conf.getString("store.path.recipes") )
+  val pathBatches = Path( conf.getString("store.path.batches") )
+
   test("store"):
-    true
+    val store = Store(pathRecipes, pathBatches)
+
+    val recipe = Recipe.default
+    store.writeRecipe(pathRecipes, recipe)
+    
+    val batch = Batch.default
+    store.writeBatch(pathBatches, batch)
