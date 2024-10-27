7a115d96d9f1807e1f0954ba8c035b5cc434704e
objektwerks <objektwerks@runbox.com>
2024-10-27 16:59:29 -0400

    enhanced store test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -5,7 +5,7 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import os.Path
+import os.*
 
 final class StoreTest extends AnyFunSuite with Matchers:
   val conf = ConfigFactory.load("/test.conf")
@@ -17,6 +17,8 @@ final class StoreTest extends AnyFunSuite with Matchers:
 
     val recipe = Recipe.default
     store.writeRecipe(pathRecipes / s"${recipe.created}.json", recipe)
+    store.readRecipe(pathRecipes / s"${recipe.created}.json") shouldBe recipe
     
     val batch = Batch.default
     store.writeBatch(pathBatches / s"${batch.created}.json", batch)
+    store.readBatch(pathBatches / s"${batch.created}.json") shouldBe batch
