527325b8485dcc707707d18d0b711b491dd7f1bf
objektwerks <objektwerks@runbox.com>
2024-10-29 12:45:20 -0400

    added conf to app

--- src/main/scala/objektwerks/ui/App.scala
+++ src/main/scala/objektwerks/ui/App.scala
@@ -1,9 +1,13 @@
 package objektwerks.ui
 
+import com.typesafe.config.ConfigFactory
+
 import scalafx.application.JFXApp3
 import scalafx.scene.image.Image
 
 object App extends JFXApp3:
+  val conf = ConfigFactory.load("app.conf")
+
   override def start(): Unit =
     stage = new JFXApp3.PrimaryStage:
       scene = View.scene
