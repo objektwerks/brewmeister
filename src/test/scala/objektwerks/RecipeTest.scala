f7c427426c6b54cda045efde1ae57e1650ba8218
objektwerks <objektwerks@runbox.com>
2024-10-29 13:50:45 -0400

    edited recipe test

--- src/test/scala/objektwerks/RecipeTest.scala
+++ src/test/scala/objektwerks/RecipeTest.scala
@@ -9,4 +9,5 @@ final class RecipeTest extends AnyFunSuite with Matchers:
   test("recipe"):
     val recipe = Recipe.default
     val recipeAsJson = write(recipe)
+    println(recipeAsJson)
     recipe shouldBe read[Recipe](recipeAsJson)
