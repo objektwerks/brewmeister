95b50a95013d19b2bd0d023926646ae383b1055b
objektwerks <objektwerks@runbox.com>
2024-10-29 13:03:49 -0400

    enhanced context

--- src/main/scala/objektwerks/ui/Context.scala
+++ src/main/scala/objektwerks/ui/Context.scala
@@ -9,10 +9,9 @@ final class Context(config: Config):
   val windowWidth = config.getDouble("window.width")
   val windowHeight = config.getDouble("window.height")
 
-  def logoImage = loadImageView("/logo.png")
-  def addImage = loadImageView("/add.png")
-
-  def logo = new Image(Image.getClass.getResourceAsStream("/logo.png"))
+  def logoImageView = loadImageView("/logo.png")
+  def addImageView = loadImageView("/add.png")
+  def logoImage = new Image(Image.getClass.getResourceAsStream("/logo.png"))
 
   private def loadImageView(path: String): ImageView = new ImageView:
     image = new Image(Image.getClass.getResourceAsStream(path))
