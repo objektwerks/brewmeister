a99234007c651889be6416afa952391c42b9279f
objektwerks <objektwerks@runbox.com>
2024-10-29 13:11:17 -0400

    enhanced model

--- src/main/scala/objektwerks/ui/Model.scala
+++ src/main/scala/objektwerks/ui/Model.scala
@@ -2,7 +2,8 @@ package objektwerks.ui
 
 import scalafx.beans.property.ObjectProperty
 
-import objektwerks.Recipe
+import objektwerks.{Batch, Recipe}
 
 final class Model:
   val objectRecipe = ObjectProperty[Recipe](Recipe())
+  val objectBatch = ObjectProperty[Batch](Batch())
