bd227f730202739783f38cf0c5787aeb69947295
objektwerks <objektwerks@runbox.com>
2024-10-29 14:07:11 -0400

    edited batch test

--- src/test/scala/objektwerks/BatchTest.scala
+++ src/test/scala/objektwerks/BatchTest.scala
@@ -9,5 +9,4 @@ final class BatchTest extends AnyFunSuite with Matchers:
   test("batch"):
     val batch = Batch.default
     val batchAsJson = write(batch)
-    println(batchAsJson)
     batch shouldBe read[Batch](batchAsJson)
