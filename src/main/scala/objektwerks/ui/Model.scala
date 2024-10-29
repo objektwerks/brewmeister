9a2298e10713763537528f77b8d5f43699fb2e03
objektwerks <objektwerks@runbox.com>
2024-10-29 14:40:46 -0400

    refactored model

--- src/main/scala/objektwerks/ui/Model.scala
+++ src/main/scala/objektwerks/ui/Model.scala
@@ -1,12 +1,13 @@
 package objektwerks.ui
 
 import scalafx.beans.property.ObjectProperty
+import scalafx.collections.ObservableBuffer
 
 import objektwerks.{Batch, Recipe}
 
 final class Model:
-  val objectRecipe = ObjectProperty[Recipe](Recipe())
-  val objectBatch = ObjectProperty[Batch](Batch())
+  val objectRecipe = ObservableBuffer[Recipe]()
+  val objectBatch = ObservableBuffer[Batch]()
 
   val selectedRecipe = ObjectProperty[String]("")
   val selectedBatch = ObjectProperty[String]("")
