aac3836777430319e04caf725fc92a3f3f792d50
objektwerks <objektwerks@runbox.com>
2024-10-31 06:50:19 -0400

    enhanced brewer test

--- src/test/scala/objektwerks/BrewerTest.scala
+++ src/test/scala/objektwerks/BrewerTest.scala
@@ -29,6 +29,7 @@ class BrewerTest extends AnyFunSuite with Matchers:
     brewer.handle( LogFermentingTempFinalGravity(72, 1.012) )
     brewer.handle( Condition(recipe) )
     brewer.handle( LogConditioningTempSrmColor(72, 9) )
+    brewer.handle( Keg(recipe) )
 
     val batch = listener.ask( _.batch )
     batch.recipe.nonEmpty shouldBe true
