186d0cf0dbe4b299ffe58a0705e4d939b5553240
objektwerks <objektwerks@runbox.com>
2024-10-31 08:58:57 -0400

    added simulate to brewer

--- src/main/scala/objektwerks/Brewer.scala
+++ src/main/scala/objektwerks/Brewer.scala
@@ -42,6 +42,32 @@ final class Brewer(listener: Listener):
       case logKeggingTempBrewhouseEfficiency: LogKeggingTempBrewhouseEfficiency =>
         Kegger(listener).logKeggingTempBrewhouseEfficiency( logKeggingTempBrewhouseEfficiency )
 
+def simulate(recipe: Recipe): Batch =
+  val listener = Listener()
+  val brewer = Brewer(listener)
+
+  brewer.brew( Sanitize(recipe) )
+  brewer.brew( Prepare(recipe) )
+  brewer.brew( Malt(recipe) )
+  brewer.brew( Mill(recipe) )
+  brewer.brew( Mash(recipe) )
+  brewer.brew( LogMashingTempPh(150, 5.6) )
+  brewer.brew( Lauter(recipe) )
+  brewer.brew( Sparge(recipe) )
+  brewer.brew( LogMashEfficiency(recipe, 4.0) )
+  brewer.brew( Boil(recipe) )
+  brewer.brew( Cool(recipe) )
+  brewer.brew( Whirlpool(recipe) )
+  brewer.brew( LogBoilingCoolingTempOriginalGravity(150, 72, 1.060) )
+  brewer.brew( Ferment(recipe) )
+  brewer.brew( LogFermentingTempFinalGravity(72, 1.012) )
+  brewer.brew( Condition(recipe) )
+  brewer.brew( LogConditioningTempSrmColor(72, 9) )
+  brewer.brew( Keg(recipe) )
+  brewer.brew( LogKeggingTempBrewhouseEfficiency(recipe, 72, 4.0) )
+
+  listener.batch
+
 final class Sanitizer(listener: Listener):
   def sanitize(sanitize: Sanitize): Unit =
     listener.onEvent(
