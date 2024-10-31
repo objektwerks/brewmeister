80eb6baf730b8191a92456b79286d51d174748fa
objektwerks <objektwerks@runbox.com>
2024-10-31 09:08:09 -0400

    simplified brewer test

--- src/test/scala/objektwerks/BrewerTest.scala
+++ src/test/scala/objektwerks/BrewerTest.scala
@@ -5,10 +5,7 @@ import org.scalatest.matchers.should.Matchers
 
 class BrewerTest extends AnyFunSuite with Matchers:
   test("brew"):
-    val recipe = Recipe.default
     val brewer = Brewer(Listener())
-    val batch = brewer.simulate(recipe)
-    val store = Store()
-    store.writeRecipe(recipe)
-    store.writeBatch(batch)
+    val batch = brewer.simulate(Recipe.default)
+    Store().writeBatch(batch)
     batch.recipe.nonEmpty shouldBe true
