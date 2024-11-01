dbec2f1f627790492d6bcce1c7a21c82d28855b5
objektwerks <objektwerks@runbox.com>
2024-11-01 02:52:58 -0400

    enhanced brewer test

--- src/test/scala/objektwerks/BrewerTest.scala
+++ src/test/scala/objektwerks/BrewerTest.scala
@@ -3,7 +3,14 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import upickle.default.*
+
 class BrewerTest extends AnyFunSuite with Matchers:
+  test("batch"):
+    val batch = Batch.default
+    val batchAsJson = write(batch)
+    batch shouldBe read[Batch](batchAsJson)
+
   test("brew"):
     val batch = Brewer.simulate(Recipe.default)
     Store().writeBatch(batch)
