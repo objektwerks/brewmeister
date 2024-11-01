92d89da20508b35eb74258c361e7a345970f8ae8
objektwerks <objektwerks@runbox.com>
2024-11-01 02:44:29 -0400

    refactored brewer simulate

--- src/main/scala/objektwerks/Brewer.scala
+++ src/main/scala/objektwerks/Brewer.scala
@@ -1,5 +1,30 @@
 package objektwerks
 
+object Brewer:
+  def simulate(recipe: Recipe): Batch =
+    val listener = Listener()
+    val brewer = Brewer(listener)
+    brewer.brew( Sanitize(recipe) )
+    brewer.brew( Prepare(recipe) )
+    brewer.brew( Malt(recipe) )
+    brewer.brew( Mill(recipe) )
+    brewer.brew( Mash(recipe) )
+    brewer.brew( LogMashingTempPh(150, 5.6) )
+    brewer.brew( Lauter(recipe) )
+    brewer.brew( Sparge(recipe) )
+    brewer.brew( LogMashEfficiency(recipe, 4.0) )
+    brewer.brew( Boil(recipe) )
+    brewer.brew( Cool(recipe) )
+    brewer.brew( Whirlpool(recipe) )
+    brewer.brew( LogBoilingCoolingTempOriginalGravity(150, 72, 1.060) )
+    brewer.brew( Ferment(recipe) )
+    brewer.brew( LogFermentingTempFinalGravity(72, 1.012) )
+    brewer.brew( Condition(recipe) )
+    brewer.brew( LogConditioningTempSrmColor(72, 9) )
+    brewer.brew( Keg(recipe) )
+    brewer.brew( LogKeggingTempBrewhouseEfficiency(recipe, 72, 4.0) )
+    listener.batch
+
 final class Brewer(listener: Listener):
   def brew(command: Command): Unit =
     command match
@@ -41,28 +66,6 @@ final class Brewer(listener: Listener):
         Kegger(listener).keg( keg )
       case logKeggingTempBrewhouseEfficiency: LogKeggingTempBrewhouseEfficiency =>
         Kegger(listener).logKeggingTempBrewhouseEfficiency( logKeggingTempBrewhouseEfficiency )
-
-  def simulate(recipe: Recipe): Batch =
-    brew( Sanitize(recipe) )
-    brew( Prepare(recipe) )
-    brew( Malt(recipe) )
-    brew( Mill(recipe) )
-    brew( Mash(recipe) )
-    brew( LogMashingTempPh(150, 5.6) )
-    brew( Lauter(recipe) )
-    brew( Sparge(recipe) )
-    brew( LogMashEfficiency(recipe, 4.0) )
-    brew( Boil(recipe) )
-    brew( Cool(recipe) )
-    brew( Whirlpool(recipe) )
-    brew( LogBoilingCoolingTempOriginalGravity(150, 72, 1.060) )
-    brew( Ferment(recipe) )
-    brew( LogFermentingTempFinalGravity(72, 1.012) )
-    brew( Condition(recipe) )
-    brew( LogConditioningTempSrmColor(72, 9) )
-    brew( Keg(recipe) )
-    brew( LogKeggingTempBrewhouseEfficiency(recipe, 72, 4.0) )
-    listener.batch
 
 final class Sanitizer(listener: Listener):
   def sanitize(sanitize: Sanitize): Unit =
