2b80d3e2f0da32c3daa83f9dfa6825301063443b
objektwerks <objektwerks@runbox.com>
2024-11-01 12:00:14 -0400

    refactored brewer test

--- src/test/scala/objektwerks/BrewerTest.scala
+++ src/test/scala/objektwerks/BrewerTest.scala
@@ -5,20 +5,8 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.annotation.nowarn
 
-import upickle.default.*
-
 @nowarn
 class BrewerTest extends AnyFunSuite with Matchers:
-  test("recipe"):
-    val recipe = Recipe.default
-    val recipeAsJson = write(recipe)
-    recipe shouldBe read[Recipe](recipeAsJson)
-
-  test("batch"):
-    val batch = Batch.default
-    val batchAsJson = write(batch)
-    batch shouldBe read[Batch](batchAsJson)
-
   test("brew > store"):
     val recipe = Recipe.default
     val batch = Brewer.simulate(recipe)
@@ -26,9 +14,9 @@ class BrewerTest extends AnyFunSuite with Matchers:
     val store = Store()
 
     store.writeRecipe(recipe)
-    assert( store.readRecipe(recipe.name) == recipe )
-    assert( store.listRecipes.length >= 1 )
+    store.readRecipe(recipe.name) shouldBe recipe
+    store.listRecipes.length should be >= 1
 
     store.writeBatch(batch)
-    assert( store.readBatch(batch.recipe, batch.started) == batch )
-    assert( store.listBatches.length >= 1 )
+    store.readBatch(batch.recipe, batch.started) shouldBe batch
+    store.listBatches.length should be >= 1
