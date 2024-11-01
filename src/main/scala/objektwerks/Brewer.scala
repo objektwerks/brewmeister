49da3343566c933859d2e823ffd9375c6ebd42b0
objektwerks <objektwerks@runbox.com>
2024-11-01 07:54:35 -0400

    enhanced brewer stimulate

--- src/main/scala/objektwerks/Brewer.scala
+++ src/main/scala/objektwerks/Brewer.scala
@@ -8,7 +8,8 @@ object Brewer:
                boilingTemp: Int = 150,
                coolingTemp: Int = 72,
                originalGravity: Double = 1.060,
-               fermentingTemp: Int = 72): Batch =
+               fermentingTemp: Int = 72,
+               finalGravity: Double = 1.012): Batch =
     val listener = Listener()
     val brewer = Brewer(listener)
     brewer.brew( Sanitize(recipe) )
