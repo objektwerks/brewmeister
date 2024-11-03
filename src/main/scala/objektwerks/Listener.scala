0f3b112b26fe59017daef938f7f1635fb80ca8c4
objektwerks <objektwerks@runbox.com>
2024-11-03 14:54:43 -0500

    enhanced listener batch

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -33,8 +33,8 @@ final class Listener:
     events.foreach: event =>
       event match
         case Sanitizing(log, started) =>
-          batch = batch.copy(log = batch.log ++ log)
-          batch = batch.copy(process = batch.process.copy(sanitizeStarted = started))
+          batch = batch.copy(process = batch.process.copy(sanitizeStarted = started),
+                             log = batch.log ++ log)
         case Sanitized(log, completed) =>
           batch = batch.copy(log = batch.log ++ log)
           batch = batch.copy(process = batch.process.copy(sanitizeCompleted = completed))
