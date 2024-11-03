876df623de0f686e49c6b084e63a9bf6d3813f4e
objektwerks <objektwerks@runbox.com>
2024-11-03 15:03:53 -0500

    enhanced listener batch

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -97,8 +97,8 @@ final class Listener:
           batch = batch.copy(process = batch.process.copy(coolCompleted = completed),
                              log = batch.log ++ log)
         case Whirlpooling(log, started) =>
-          batch = batch.copy(log = batch.log ++ log)
-          batch = batch.copy(process = batch.process.copy(whirlpoolStarted = started))
+          batch = batch.copy(process = batch.process.copy(whirlpoolStarted = started),
+                             log = batch.log ++ log)
         case Whirlpooled(log, completed) =>
           batch = batch.copy(log = batch.log ++ log)
           batch = batch.copy(process = batch.process.copy(whirlpoolCompleted = completed))
