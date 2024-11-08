058b7bbff49e01d141203cd663d937c56dd138c7
objektwerks <objektwerks@runbox.com>
2024-11-08 10:11:25 -0500

    refactored batch pane

--- src/main/scala/objektwerks/ui/pane/BatchPane.scala
+++ src/main/scala/objektwerks/ui/pane/BatchPane.scala
@@ -3,10 +3,9 @@ package objektwerks.ui.pane
 import scalafx.scene.control.{Tab, TabPane}
 import scalafx.scene.layout.{Priority, VBox}
 
-import objektwerks.Batch
-import objektwerks.ui.Context
+import objektwerks.ui.{Context, Model}
 
-final class BatchPane(context: Context, batch: Batch) extends TabPane:
+final class BatchPane(context: Context, model: Model) extends TabPane:
   val batchTab = new Tab:
   	text = context.tabBatch
   	closable = false
