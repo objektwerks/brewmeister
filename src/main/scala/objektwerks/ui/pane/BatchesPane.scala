2161779e80d9abc7712d96a2c02b6f133627f03a
objektwerks <objektwerks@runbox.com>
2024-11-08 09:47:50 -0500

    enhanced batches pane

--- src/main/scala/objektwerks/ui/pane/BatchesPane.scala
+++ src/main/scala/objektwerks/ui/pane/BatchesPane.scala
@@ -1,7 +1,16 @@
 package objektwerks.ui.pane
 
-import scalafx.scene.control.TabPane
+import scalafx.scene.control.{Tab, TabPane}
+import scalafx.scene.layout.{Priority, VBox}
 
 import objektwerks.ui.Context
 
-final class BatchesPane(context: Context) extends TabPane
+final class BatchesPane(context: Context) extends TabPane:
+  val recipesTab = new Tab:
+  	text = "Recipes"
+  	closable = false
+  	content = RecipesPane(context)
+
+  tabs = List(recipesTab)
+
+  VBox.setVgrow(this, Priority.Always)
