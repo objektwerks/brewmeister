fa2a9aa54a56d745920c1b1b40e7705fab70fe3d
objektwerks <objektwerks@runbox.com>
2024-11-08 10:10:33 -0500

    edited right pane

--- src/main/scala/objektwerks/ui/pane/RightPane.scala
+++ src/main/scala/objektwerks/ui/pane/RightPane.scala
@@ -2,7 +2,7 @@ package objektwerks.ui.pane
 
 import scalafx.scene.layout.HBox
 
-import objektwerks.ui.{Context, Model}
+import objektwerks.ui.{Context, Model}e
 
 final class RightPane(context: Context, model: Model) extends HBox:
   children = List(RecipePane(context, model), BatchPane(context, model))
