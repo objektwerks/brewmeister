cfe84855878a1eb1f76307eaa2791f5852541d37
objektwerks <objektwerks@runbox.com>
2024-11-08 09:23:51 -0500

    enhanced view

--- src/main/scala/objektwerks/ui/View.scala
+++ src/main/scala/objektwerks/ui/View.scala
@@ -1,13 +1,26 @@
 package objektwerks.ui
 
+import scalafx.Includes.*
+
+import scalafx.geometry.Orientation
 import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
+import javafx.scene.control.SplitPane
+import scalafx.scene.layout.{Priority, VBox}
+
+import pane.{LeftPane, RightPane}
 
 final class View(context: Context):
+  val splitPane = new SplitPane:
+    orientation = Orientation.Horizontal
+    items.addAll(LeftPane(), RightPane())
+
+  splitPane.setDividerPositions(0.20, 0.80)
+  VBox.setVgrow(splitPane, Priority.Always)
+
   val rootBox = new VBox:
     prefWidth = context.windowWidth
     prefHeight = context.windowHeight
-    children = List()
+    children = List(splitPane)
 
   val scene = new Scene:
     root = rootBox
