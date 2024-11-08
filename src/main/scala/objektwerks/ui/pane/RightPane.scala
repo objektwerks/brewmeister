14699209b2278c7416a8e01ddc2a97ff38b2ea74
objektwerks <objektwerks@runbox.com>
2024-11-08 10:57:37 -0500

    enhanced right pane

--- src/main/scala/objektwerks/ui/pane/RightPane.scala
+++ src/main/scala/objektwerks/ui/pane/RightPane.scala
@@ -1,8 +1,11 @@
 package objektwerks.ui.pane
 
 import scalafx.scene.control.TabPane
+import scalafx.scene.layout.{Priority, VBox}
 
 import objektwerks.ui.{Context, Model}
 
 final class RightPane(context: Context, model: Model) extends TabPane:
   tabs = List(RecipePane(context, model), BatchPane(context, model))
+
+  VBox.setVgrow(this, Priority.Always)
