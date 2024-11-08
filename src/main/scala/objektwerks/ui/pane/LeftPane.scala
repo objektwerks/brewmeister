f2e8d05e3a72f08d9bc60b527764f73f1a4128d9
objektwerks <objektwerks@runbox.com>
2024-11-08 09:39:55 -0500

    enhanced left pane

--- src/main/scala/objektwerks/ui/pane/LeftPane.scala
+++ src/main/scala/objektwerks/ui/pane/LeftPane.scala
@@ -4,4 +4,4 @@ import scalafx.scene.layout.VBox
 import objektwerks.ui.Context
 
 final class LeftPane(context: Context) extends VBox:
-  children = List(RecipesPane(), BatchesPane())
+  children = List(RecipesPane(context), BatchesPane(context))
