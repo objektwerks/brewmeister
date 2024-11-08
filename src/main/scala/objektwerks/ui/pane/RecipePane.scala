e3fe55415836d7c925683f2f803687b5384d0781
objektwerks <objektwerks@runbox.com>
2024-11-08 10:03:40 -0500

    added recipe to recipe pane

--- src/main/scala/objektwerks/ui/pane/RecipePane.scala
+++ src/main/scala/objektwerks/ui/pane/RecipePane.scala
@@ -3,13 +3,13 @@ package objektwerks.ui.pane
 import scalafx.scene.control.{Tab, TabPane}
 import scalafx.scene.layout.{Priority, VBox}
 
+import objektwerks.Recipe
 import objektwerks.ui.Context
 
-final class RecipePane(context: Context) extends TabPane:
+final class RecipePane(context: Context, recipe: Recipe) extends TabPane:
   val recipeTab = new Tab:
   	text = context.tabRecipe
   	closable = false
-  	content = RecipePane(context)
 
   tabs = List(recipeTab)
 
