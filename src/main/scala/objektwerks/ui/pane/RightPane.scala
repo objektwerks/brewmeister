c3773085b311aa6d71ca45d8561d2916c7932775
objektwerks <objektwerks@runbox.com>
2024-11-08 09:31:17 -0500

    enhanced right pane

--- src/main/scala/objektwerks/ui/pane/RightPane.scala
+++ src/main/scala/objektwerks/ui/pane/RightPane.scala
@@ -1,5 +1,6 @@
 package objektwerks.ui.pane
 
-import scalafx.scene.layout.VBox
+import scalafx.scene.layout.HBox
 
-final class RightPane extends VBox
+final class RightPane extends HBox:
+  children = List(RecipePane(), BatchPane())
