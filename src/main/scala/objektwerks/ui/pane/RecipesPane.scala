d6aec265fe61923caa4ef5196207a309ffcb9cec
objektwerks <objektwerks@runbox.com>
2024-11-10 06:19:24 -0500

    enhanced recipes pane

--- src/main/scala/objektwerks/ui/pane/RecipesPane.scala
+++ src/main/scala/objektwerks/ui/pane/RecipesPane.scala
@@ -1,11 +1,20 @@
 package objektwerks.ui.pane
 
-import scalafx.scene.control.{Tab, TabPane}
+import scalafx.scene.control.{Tab, TabPane, TableColumn, TableView}
 import scalafx.scene.layout.{Priority, VBox}
 
+import objektwerks.Recipe
 import objektwerks.ui.{Context, Model}
 
 final class RecipesPane(context: Context, model: Model) extends TabPane:
+  private val tableView = new TableView[Recipe]():
+    columns ++= List(
+      new TableColumn[Recipe, String]:
+        text = "Recipe"
+        cellValueFactory = _.value.name
+    )
+    items = model.observableRecipes
+
   private val recipesTab = new Tab:
   	text = context.tabRecipes
   	closable = false
