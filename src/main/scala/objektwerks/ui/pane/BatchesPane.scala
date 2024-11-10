52151021214dd87fccc6370fbc582d580fcb14f0
objektwerks <objektwerks@runbox.com>
2024-11-10 06:43:20 -0500

    added table view to batches pane

--- src/main/scala/objektwerks/ui/pane/BatchesPane.scala
+++ src/main/scala/objektwerks/ui/pane/BatchesPane.scala
@@ -1,11 +1,20 @@
 package objektwerks.ui.pane
 
-import scalafx.scene.control.{Tab, TabPane}
+import scalafx.scene.control.{Tab, TabPane, TableColumn, TableView}
 import scalafx.scene.layout.{Priority, VBox}
 
+import objektwerks.Batch
 import objektwerks.ui.{Context, Model}
 
 final class BatchesPane(context: Context, model: Model) extends TabPane:
+  val tableView = new TableView[Batch]():
+    columns ++= List(
+      new TableColumn[Batch, String]:
+        text = context.tabBatch
+        cellValueFactory = _.value.recipeProperty
+    )
+    items = model.observableBatches
+
   val tab = new Tab:
   	text = context.tabBatches
   	closable = false
