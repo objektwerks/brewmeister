398f3d2952cf3676f02b828d6201b55eb40a89de
objektwerks <objektwerks@runbox.com>
2024-11-10 06:47:26 -0500

    edited recipes pane

--- src/main/scala/objektwerks/ui/pane/RecipesPane.scala
+++ src/main/scala/objektwerks/ui/pane/RecipesPane.scala
@@ -11,7 +11,7 @@ final class RecipesPane(context: Context, model: Model) extends TabPane:
   val tableView = new TableView[Recipe]():
     columns ++= List(
       new TableColumn[Recipe, String]:
-        text = context.tabRecipe
+        text = context.columnRecipe
         cellValueFactory = _.value.nameProperty
     )
     items = model.observableRecipes
