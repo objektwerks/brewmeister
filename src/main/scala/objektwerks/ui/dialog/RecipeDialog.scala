78387a899144a4f4ad7c98997df4879be43a9413
objektwerks <objektwerks@runbox.com>
2024-11-11 09:17:32 -0500

    enhanced recipe dialog

--- src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
@@ -3,5 +3,7 @@ package objektwerks.ui.dialog
 import scalafx.scene.control.Dialog
 
 import objektwerks.Recipe
+import objektwerks.ui.App
 
-final class RecipeDialog extends Dialog[Recipe]
+final class RecipeDialog extends Dialog[Recipe]:
+  initOwner(App.stage)
