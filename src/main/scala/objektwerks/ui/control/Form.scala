0a7d32c6c9fbb01a8b700f633be5214f0ca9d85c
objektwerks <objektwerks@runbox.com>
2024-11-12 11:05:38 -0500

    added form

---
+++ src/main/scala/objektwerks/ui/control/Form.scala
@@ -0,0 +1,19 @@
+package objektwerks.ui.control
+
+import scalafx.geometry.Insets
+import scalafx.scene.Node
+import scalafx.scene.control.Label
+import scalafx.scene.layout.{GridPane, Priority}
+
+final class Form(fields: List[(Label, Node)]) extends GridPane:
+  hgap = 6
+  vgap = 6
+  padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
+  GridPane.setHgrow(this, Priority.Always)
+  GridPane.setVgrow(this, Priority.Always)
+
+  var row = 0
+  for ((label, node) <- fields)
+    add(label, columnIndex = 0, rowIndex = row)
+    add(node, columnIndex = 1, rowIndex = row)
+    row += 1
