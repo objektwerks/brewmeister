d340f780596438c221596fb5053452e692149b29
objektwerks <objektwerks@runbox.com>
2024-11-12 11:35:15 -0500

    added buttons to recipe dialog

--- src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
@@ -1,6 +1,8 @@
 package objektwerks.ui.dialog
 
-import scalafx.scene.control.Dialog
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.control.ButtonBar.ButtonData
 
 import objektwerks.Recipe
 import objektwerks.ui.{App, Context}
@@ -9,3 +11,6 @@ final class RecipeDialog(context: Context, recipe: Recipe) extends Dialog[Recipe
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogRecipe
+
+  val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
+  dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
