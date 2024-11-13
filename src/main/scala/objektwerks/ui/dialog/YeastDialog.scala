0a89a5ec4ed73b1472f26a5e0d53bc369461acad
objektwerks <objektwerks@runbox.com>
2024-11-13 08:38:51 -0500

    added yeast dialog

---
+++ src/main/scala/objektwerks/ui/dialog/YeastDialog.scala
@@ -0,0 +1,21 @@
+package objektwerks.ui.dialog
+
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.control.ButtonBar.ButtonData
+
+import objektwerks.Yeast
+import objektwerks.ui.{App, Context}
+
+final class YeastDialog(context: Context, yeast: Yeast) extends Dialog[Yeast]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.dialogGrain
+
+  val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
+  dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
+
+  resultConverter = dialogButton =>
+    if dialogButton == saveButtonType then
+      yeast // TODO
+    else null
