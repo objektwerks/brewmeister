1dd6f6ddeeeb55d8a4085a39db7e12c8ec32975f
objektwerks <objektwerks@runbox.com>
2024-11-15 13:56:00 -0500

    enhanced recipe dialog

--- src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
@@ -1,33 +1,39 @@
 package objektwerks.ui.dialog
 
 import scalafx.Includes.*
-import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.control.{ButtonType, Dialog, Label}
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.layout.Region
 
 import objektwerks.Recipe
 import objektwerks.ui.{App, Context}
-import objektwerks.ui.control.NonEmptyTextField
+import objektwerks.ui.control.{ControlGrid, NonEmptyTextField}
+import scalafx.scene.Node
 
 final class RecipeDialog(context: Context, recipe: Recipe) extends Dialog[Recipe]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogRecipe
 
+  val labelName = Label( context.labelName )
   val nameTextField = new NonEmptyTextField:
     text = recipe.name
 
+  val labelStyle = Label( context.labelStyle )
   val styleTextField = new NonEmptyTextField:
     text = recipe.style
 
+  val labelWater = Label( context.labelWater )
   val waterTextField = new NonEmptyTextField:
     text = recipe.water
 
-  val controls = List[(String, Region)](
-    context.labelName -> nameTextField,
-    context.labelStyle -> styleTextField,
-    context.labelWater -> waterTextField
+  val controls = List[(Label, Node)](
+    labelName -> nameTextField,
+    labelStyle -> styleTextField,
+    labelWater -> waterTextField
   )
+
+  dialogPane().content = ControlGrid(controls)
 
   val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
   dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
