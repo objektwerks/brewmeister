232cf0afa7cd17d1ef97ef51bd639828d5bee2a1
objektwerks <objektwerks@runbox.com>
2024-11-15 14:51:02 -0500

    added volume dialog

---
+++ src/main/scala/objektwerks/ui/dialog/VolumeDialog.scala
@@ -0,0 +1,21 @@
+package objektwerks.ui.dialog
+
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.control.ButtonBar.ButtonData
+
+import objektwerks.Volume
+import objektwerks.ui.{App, Context}
+
+final class VolumeDialog(context: Context, volume: Volume) extends Dialog[Volume]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.dialogGrain
+
+  val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
+  dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
+
+  resultConverter = dialogButton =>
+    if dialogButton == saveButtonType then
+      volume // TODO
+    else null
