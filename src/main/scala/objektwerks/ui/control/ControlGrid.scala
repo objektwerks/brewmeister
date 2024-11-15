0c4b414dfb84168d53fbe619c056bbd0ad7cb912
objektwerks <objektwerks@runbox.com>
2024-11-15 13:56:52 -0500

    edited control grid

--- src/main/scala/objektwerks/ui/control/ControlGrid.scala
+++ src/main/scala/objektwerks/ui/control/ControlGrid.scala
@@ -5,7 +5,7 @@ import scalafx.scene.Node
 import scalafx.scene.control.Label
 import scalafx.scene.layout.{GridPane, Priority}
 
-final class ControlGrid(fields: List[(Label, Node)]) extends GridPane:
+final class ControlGrid(controls: List[(Label, Node)]) extends GridPane:
   hgap = 6
   vgap = 6
   padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
@@ -13,7 +13,7 @@ final class ControlGrid(fields: List[(Label, Node)]) extends GridPane:
   GridPane.setVgrow(this, Priority.Always)
 
   var row = 0
-  for ((label, node) <- fields)
+  for ((label, node) <- controls)
     add(label, columnIndex = 0, rowIndex = row)
     add(node, columnIndex = 1, rowIndex = row)
     row += 1
