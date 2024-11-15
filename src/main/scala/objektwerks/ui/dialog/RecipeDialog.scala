d7ac0de110a7d9cf2dde1a467bff690fb81a231f
objektwerks <objektwerks@runbox.com>
2024-11-15 13:44:42 -0500

    enhanced recipe dialog

--- src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
@@ -6,11 +6,15 @@ import scalafx.scene.control.ButtonBar.ButtonData
 
 import objektwerks.Recipe
 import objektwerks.ui.{App, Context}
+import objektwerks.ui.control.NonEmptyTextField
 
 final class RecipeDialog(context: Context, recipe: Recipe) extends Dialog[Recipe]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogRecipe
+
+  val nameTextField = new NonEmptyTextField:
+    text = recipe.name
 
   val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
   dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
