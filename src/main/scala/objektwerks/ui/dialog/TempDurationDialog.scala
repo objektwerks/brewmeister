71b984338c9ce343133cc21230c1907232637f66
objektwerks <objektwerks@runbox.com>
2024-11-15 14:52:51 -0500

    added temp duration dialog

---
+++ src/main/scala/objektwerks/ui/dialog/TempDurationDialog.scala
@@ -0,0 +1,21 @@
+package objektwerks.ui.dialog
+
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.control.ButtonBar.ButtonData
+
+import objektwerks.TempDuration
+import objektwerks.ui.{App, Context}
+
+final class TempDurationDialog(context: Context, tempDuration: TempDuration) extends Dialog[TempDuration]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.dialogGrain
+
+  val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
+  dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
+
+  resultConverter = dialogButton =>
+    if dialogButton == saveButtonType then
+      tempDuration // TODO
+    else null
