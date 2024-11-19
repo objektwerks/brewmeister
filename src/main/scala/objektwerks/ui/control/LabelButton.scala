281369159411965cd436291a411778e7dcb03c20
objektwerks <objektwerks@runbox.com>
2024-11-19 05:01:24 -0500

    enhanced label button

--- src/main/scala/objektwerks/ui/control/LabelButton.scala
+++ src/main/scala/objektwerks/ui/control/LabelButton.scala
@@ -1,13 +1,12 @@
 package objektwerks.ui.control
 
-import scalafx.geometry.{Insets, Pos}
+import scalafx.geometry.Pos
 import scalafx.scene.control.{Button, Label}
 import scalafx.scene.layout.HBox
 
 final class LabelButton(labelText: String,
                         buttonAction: () => Unit) extends HBox:
   spacing = 6
-  padding = Insets(6)
 
   val label = new Label:
     alignment = Pos.CenterLeft
