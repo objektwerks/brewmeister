2be4c0fd1e19c07e9c2558c7f6bb93339a91c348
objektwerks <objektwerks@runbox.com>
2024-11-19 04:44:31 -0500

    enhanced label button

--- src/main/scala/objektwerks/ui/control/LabelButton.scala
+++ src/main/scala/objektwerks/ui/control/LabelButton.scala
@@ -1,5 +1,10 @@
 package objektwerks.ui.control
 
+import scalafx.geometry.Pos
+import scalafx.scene.control.Label
 import scalafx.scene.layout.HBox
 
-final class LabelButton extends HBox
+final class LabelButton(label: String) extends HBox:
+  val l = new Label:
+    alignment = Pos.CenterLeft
+    text = label
