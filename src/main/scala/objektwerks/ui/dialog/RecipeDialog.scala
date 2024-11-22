26675cc9282251a99c793abeae0b75c99bc50cf1
objektwerks <objektwerks@runbox.com>
2024-11-22 05:19:38 -0500

    enhanced recipe dialog

--- src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/RecipeDialog.scala
@@ -224,6 +224,7 @@ final class RecipeDialog(context: Context, recipe: Recipe) extends Dialog[Recipe
         water = textFieldWater.string,
         volume = labelButtonVolume.value.value,
         grains = labelButtonGrains.value.value,
+        hops = labelButtonHops.value.value,
         potentialMashExtract = textFieldPotentialMashExtract.double.format,
         potentialFermentableExtract = textFieldPotentialFermentableExtract.double.format,
         coolingTempRange = IntRange( rangeSliderCoolingTempRange.getLowValue.toInt, rangeSliderCoolingTempRange.getHighValue.toInt ),
