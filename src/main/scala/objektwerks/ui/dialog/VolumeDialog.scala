ebb9ee85df47f28acea04c7fb544f3612c01f815
objektwerks <objektwerks@runbox.com>
2024-11-24 14:34:40 -0500

    enhanced volume dialog

--- src/main/scala/objektwerks/ui/dialog/VolumeDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/VolumeDialog.scala
@@ -1,7 +1,7 @@
 package objektwerks.ui.dialog
 
 import scalafx.Includes.*
-import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.control.{ButtonType, Dialog, Label}
 import scalafx.scene.control.ButtonBar.ButtonData
 
 import objektwerks.Volume
@@ -11,6 +11,8 @@ final class VolumeDialog(context: Context, volume: Volume) extends Dialog[Volume
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogGrain
+
+  val labelValue = Label( context.labelValue )
 
   val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
   dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
