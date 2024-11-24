41462d08c036fd2b70a72139177c0f327e3b0c2c
objektwerks <objektwerks@runbox.com>
2024-11-24 15:10:45 -0500

    enhanced temp duration dialog

--- src/main/scala/objektwerks/ui/dialog/TempRangeDurationDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/TempRangeDurationDialog.scala
@@ -1,7 +1,7 @@
 package objektwerks.ui.dialog
 
 import scalafx.Includes.*
-import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.control.{ButtonType, Dialog, Label}
 import scalafx.scene.control.ButtonBar.ButtonData
 
 import objektwerks.TempRangeDuration
@@ -11,6 +11,8 @@ final class TempRangeDurationDialog(context: Context, tempRangeDuration: TempRan
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogGrain
+
+  val labelTempRange = Label( context.labelTempRange )
 
   val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
   dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
