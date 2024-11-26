42eda4df81dcd628d07b08bfc8b5853d3a552a74
objektwerks <objektwerks@runbox.com>
2024-11-26 07:37:28 -0500

    edited app.conf

--- src/main/scala/objektwerks/ui/Context.scala
+++ src/main/scala/objektwerks/ui/Context.scala
@@ -39,6 +39,7 @@ final class Context(config: Config):
   val labelHops = config.getString("label.hops")
   val labelAdjuncts = config.getString("label.adjuncts")
   val labelYeasts = config.getString("label.yeasts")
+  
   val labelTempRange = config.getString("label.tempRange")
   val labelDuration = config.getString("label.duration")
   val labelMashingTempRangeDuration = config.getString("label.mashingTempRangeDuration")
