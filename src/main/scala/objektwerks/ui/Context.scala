ab3280cad05f86dcda424dd20c2489765af8fba7
objektwerks <objektwerks@runbox.com>
2024-11-27 08:12:35 -0500

    enhanced context

--- src/main/scala/objektwerks/ui/Context.scala
+++ src/main/scala/objektwerks/ui/Context.scala
@@ -115,6 +115,7 @@ final class Context(config: Config):
   def editImageView = loadImageView("/image/edit.png")
   def removeImageView = loadImageView("/image/remove.png")
   def plusImage = loadImageView("/image/plus.png")
+  def minusImage = loadImageView("/image/minus.png")
   def logoImage = new Image(Image.getClass.getResourceAsStream("/image/logo.png"))
 
   private def loadImageView(path: String): ImageView = new ImageView:
