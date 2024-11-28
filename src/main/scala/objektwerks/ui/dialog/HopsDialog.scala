ef96cdc0a3a12907152577909115efbe3a477e30
objektwerks <objektwerks@runbox.com>
2024-11-28 13:37:14 -0500

    enhanced hops dialog

--- src/main/scala/objektwerks/ui/dialog/HopsDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/HopsDialog.scala
@@ -2,7 +2,7 @@ package objektwerks.ui.dialog
 
 import scalafx.Includes.*
 import scalafx.collections.ObservableBuffer
-import scalafx.scene.control.{ButtonType, Dialog, ListView}
+import scalafx.scene.control.{ButtonType, Dialog, ListView, SelectionMode}
 import scalafx.scene.control.ButtonBar.ButtonData
 
 import objektwerks.Hop
@@ -20,6 +20,11 @@ final class HopsDialog(context: Context, hops: Array[Hop]) extends Dialog[Array[
   val listViewHops = new ListView[Hop]:
     items = ObservableBuffer.from(updatedHops)
     cellFactory = (cell, hop) => cell.text = hop.name
+
+  listViewHops.selectionModel().selectionModeProperty.value = SelectionMode.Single
+  listViewHops.selectionModel().selectedItemProperty().addListener { (_, _, selectedHop) =>
+    if selectedHop != null then select(selectedHop)
+  }
 
   val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
   dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
