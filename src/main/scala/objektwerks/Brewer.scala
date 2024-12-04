ec0c8a9fc50703bea1cce22bc100e23b96dfad91
objektwerks <objektwerks@runbox.com>
2024-12-04 13:43:48 -0500

    renamed method

--- src/main/scala/objektwerks/Brewer.scala
+++ src/main/scala/objektwerks/Brewer.scala
@@ -1,23 +1,23 @@
 package objektwerks
 
 object Brewer:
-  def simulate(listener: Listener,
-               recipe: Recipe,
-               mashingTemp: Int = 150,
-               pH: Double = 5.6,
-               actualMashExtract: Double = 4.5,
-               boilingTemp: Int = 150,
-               coolingTemp: Int = 72,
-               originalGravity: Double = 1.060,
-               fermentingTemp: Int = 72,
-               finalGravity: Double = 1.012,
-               conditioningTemp: Int = 72,
-               srmColor: Int = 9,
-               keggingTemp: Int = 72,
-               appearance: Int = 3,
-               aroma: Int = 3,
-               taste: Int = 3,
-               actualFermentableExtract: Double = 4.5): Unit =
+  def run(listener: Listener,
+          recipe: Recipe,
+          mashingTemp: Int = 150,
+          pH: Double = 5.6,
+          actualMashExtract: Double = 4.5,
+          boilingTemp: Int = 150,
+          coolingTemp: Int = 72,
+          originalGravity: Double = 1.060,
+          fermentingTemp: Int = 72,
+          finalGravity: Double = 1.012,
+          conditioningTemp: Int = 72,
+          srmColor: Int = 9,
+          keggingTemp: Int = 72,
+          appearance: Int = 3,
+          aroma: Int = 3,
+          taste: Int = 3,
+          actualFermentableExtract: Double = 4.5): Unit =
     val brewer = Brewer(listener)
     brewer.brew( Sanitize(recipe) )
     brewer.brew( Prepare(recipe) )
