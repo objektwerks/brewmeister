92f24d3f287172e47f68e2b4af947efbc39c306e
objektwerks <objektwerks@runbox.com>
2024-12-05 07:01:48 -0500

    added prop to recipe

--- src/main/scala/objektwerks/Recipe.scala
+++ src/main/scala/objektwerks/Recipe.scala
@@ -79,6 +79,7 @@ final case class Recipe(name: String = "",
   val coolingTempRangeProperty = ObjectProperty[IntRange](this, "coolingTempRange", coolingTempRange)
   val fermentingTempRangeDurationProperty = ObjectProperty[TempRangeDuration](this, "fermentingTempRangeDuration", fermentingTempRangeDuration)
   val potentialFermentableExtractProperty = ObjectProperty[Double](this, "potentialFermentableExtract", potentialFermentableExtract)
+  val conditioningTempRangeDurationProperty = ObjectProperty[TempRangeDuration](this, "conditioningTempRangeDuration", conditioningTempRangeDuration)
 
   val recipe = this
 
