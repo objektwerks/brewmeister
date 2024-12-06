8797d3aa58dfba1ef724044b67eb75bc6fe850f6
objektwerks <objektwerks@runbox.com>
2024-12-06 05:53:31 -0500

    added prop to yeast

--- src/main/scala/objektwerks/Recipe.scala
+++ src/main/scala/objektwerks/Recipe.scala
@@ -163,4 +163,5 @@ final case class Yeast(name: String = Random.alphanumeric.take(7).mkString,
                        mixinMinute: Int = 1,
                        mixinStep: MixinStep = MixinStep.Fermenting) derives CanEqual, JsonSupport:
   val nameProperty = ObjectProperty[String](this, "name", name)
+  val weightProperty = ObjectProperty[Double](this, "weight", weight)
   val yeast = this
