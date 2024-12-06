e217cf535b9410d2809d3597f74165d70bf5507a
objektwerks <objektwerks@runbox.com>
2024-12-06 11:32:36 -0500

    enhanced menu

--- src/main/scala/objektwerks/ui/Menu.scala
+++ src/main/scala/objektwerks/ui/Menu.scala
@@ -1,5 +1,16 @@
 package objektwerks.ui
 
-import scalafx.scene.control.MenuBar
+import scalafx.Includes._
+import scalafx.event.ActionEvent
+import scalafx.scene.control.{Menu, MenuBar, MenuItem}
 
-final class Menu extends MenuBar
+final class Menu(context: Context) extends MenuBar:
+  val menuItemExit = new MenuItem:
+    text = context.labelAdjuncts
+    onAction = (_: ActionEvent) => System.exit(0)
+
+  val menu = new Menu():
+    text = context.labelAdjuncts
+    items = List(menuItemExit)
+
+  menus = List(menu)
