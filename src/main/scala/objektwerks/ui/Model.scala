c423caea5c5ce5ad45f6de907ab8ffed4860b5c7
objektwerks <objektwerks@runbox.com>
2024-12-13 06:16:12 -0500

    simplified model

--- src/main/scala/objektwerks/ui/Model.scala
+++ src/main/scala/objektwerks/ui/Model.scala
@@ -9,8 +9,8 @@ final class Model(store: Store):
   val observableRecipes = ObservableBuffer.from(store.listRecipes)
   val observableBatches = ObservableBuffer.from(store.listBatches)
 
-  val selectedRecipeIndex = ObjectProperty[Int](0)
+  val selectedRecipeIndex = ObjectProperty(0)
   val selectedRecipe = ObjectProperty( Recipe() )
 
-  val selectedBatchIndex = ObjectProperty[Int](0)
+  val selectedBatchIndex = ObjectProperty(0)
   val selectedBatch = ObjectProperty( Batch() )
