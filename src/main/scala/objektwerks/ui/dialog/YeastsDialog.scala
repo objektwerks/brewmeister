fadf442ef4c73b7e5b5266992596bc74f47a724d
objektwerks <objektwerks@runbox.com>
2024-12-17 16:28:07 -0500

    enhanced yeasts dialog

--- src/main/scala/objektwerks/ui/dialog/YeastsDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/YeastsDialog.scala
@@ -15,6 +15,22 @@ final class YeastsDialog(context: Context, yeasts: Array[Yeast]) extends Dialog[
   // Model
   val updatedYeasts = yeasts.map(identity).toBuffer.sorted
 
+  // Bindings
+  def adjunctToControls(adjunct: Adjunct): Unit =
+    textFieldName.text = adjunct.name
+    textFieldWeight.text = adjunct.weight.toString
+    choiceBoxUnit.value = adjunct.unit.toString
+    textFieldMixinMinute.text = adjunct.mixinMinute.toString
+    choiceBoxMixinStep.value = adjunct.mixinStep.toString
+
+  def resetControls(): Unit =
+    saveButton.disable = true
+    textFieldName.text = ""
+    textFieldWeight.text = ""
+    choiceBoxUnit.value = ""
+    textFieldMixinMinute.text = ""
+    choiceBoxMixinStep.value = ""
+
   val saveButtonType = new ButtonType(context.tooltipSave, ButtonData.OKDone)
   dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
 
