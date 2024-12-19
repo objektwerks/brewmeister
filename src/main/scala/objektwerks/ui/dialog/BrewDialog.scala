4696bde5fa549a643dcc58db84f0ae1591ef23b0
objektwerks <objektwerks@runbox.com>
2024-12-19 08:34:09 -0500

    enhanced brew dialog

--- src/main/scala/objektwerks/ui/dialog/BrewDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/BrewDialog.scala
@@ -1,15 +1,22 @@
 package objektwerks.ui.dialog
 
-import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.Node
+import scalafx.scene.control.{ButtonType, Dialog, Label}
 import scalafx.scene.control.ButtonBar.ButtonData
 
 import objektwerks.Batch
 import objektwerks.ui.{App, Context}
+import objektwerks.ui.control.ControlGrid
 
 final class BrewDialog(context: Context) extends Dialog[Batch]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogBrew
+
+  val controls = List[(Label, Node)](
+  )
+
+  dialogPane().content = ControlGrid(controls)
 
   val saveButtonType = new ButtonType(context.tooltipSave, ButtonData.OKDone)
   dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
