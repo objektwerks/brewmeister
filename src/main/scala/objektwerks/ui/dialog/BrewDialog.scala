e54adfb3acb0e2833578a55efcb6ec68d0f6f7f2
objektwerks <objektwerks@runbox.com>
2024-12-19 08:51:26 -0500

    edited app.conf

--- src/main/scala/objektwerks/ui/dialog/BrewDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/BrewDialog.scala
@@ -27,6 +27,10 @@ final class BrewDialog(context: Context, listener: Listener, recipe: Recipe) ext
   val textFieldPh = new DoubleTextField:
     text = 5.6.toString
 
+  val labelActualMashExtract = Label( context.labelActualMashExtract )
+  val textFieldActualMashExtract = new DoubleTextField:
+    text = 4.5.toString
+
   val controls = List[(Label, Node)](
     labelRecipe -> textRecipe,
     labelMashingTemmp -> textFieldMashingTemp,
