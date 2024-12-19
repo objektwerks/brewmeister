b7843f401091a2b446d07e3557995fb796a129fd
objektwerks <objektwerks@runbox.com>
2024-12-19 08:26:34 -0500

    enhanced brew dialog

--- src/main/scala/objektwerks/ui/dialog/BrewDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/BrewDialog.scala
@@ -3,7 +3,9 @@ package objektwerks.ui.dialog
 import scalafx.scene.control.Dialog
 
 import objektwerks.Batch
+import objektwerks.ui.Context
 
-final class BrewDialog extends Dialog[Batch]:
+final class BrewDialog(context: Context) extends Dialog[Batch]:
   initOwner(App.stage)
   title = context.windowTitle
+  headerText = context.dialogVolume
