4ce211c30237c938b2b535ede3cfbfc77adc9f81
objektwerks <objektwerks@runbox.com>
2024-12-20 15:58:58 -0500

    enhanced log dialog

--- src/main/scala/objektwerks/ui/dialog/LogDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/LogDialog.scala
@@ -1,10 +1,14 @@
 package objektwerks.ui.dialog
 
-import scalafx.scene.control.Dialog
+import scalafx.scene.control.{Dialog, Label, TextArea}
 
-import objektwerks.ui.{App, Context}
+import objektwerks.ui.{App, Context, Model}
 
-final class LogDialog(context: Context) extends Dialog[Unit]:
+final class LogDialog(context: Context, model: Model) extends Dialog[Unit]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogLog
+
+  val labelLog = Label( context.labelLog )
+  val textAreaLog = new TextArea( model.selectedBatch.value.log.mkString ):
+    wrapText = true
