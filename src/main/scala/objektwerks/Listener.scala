b0eef0acf08846cfab41d44ae097f50a0f99b1cb
objektwerks <objektwerks@runbox.com>
2024-12-20 14:26:04 -0500

    refactored listener

--- src/main/scala/objektwerks/Listener.scala
+++ src/main/scala/objektwerks/Listener.scala
@@ -32,123 +32,199 @@ final class Listener:
 
   def batch: Batch =
     var batch = Batch()
+    batch = batch.copy(started = now())
     events.foreach {
       case Sanitizing(log, started) =>
-        batch = batch.copy(process = batch.process.copy(sanitizingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(sanitizingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Sanitized(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(sanitizingCompleted = completed),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(sanitizingCompleted = completed),
+          log = batch.log ++ log
+        )
+
       case Preparing(log, started) =>
-        batch = batch.copy(process = batch.process.copy(preparingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(preparingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Prepared(recipe, style, volume, log, completed) =>
-        batch = batch.copy(process = batch.process.copy(preparingCompleted = completed),
+        batch = batch.copy(
+          process = batch.process.copy(preparingCompleted = completed),
           recipe = recipe,
           style = style,
           volume = volume,
-          log = batch.log ++ log)
+          log = batch.log ++ log
+        )
+
       case Malting(log, started) =>
-        batch = batch.copy(process = batch.process.copy(maltingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(maltingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Malted(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(maltingCompleted = completed),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(maltingCompleted = completed),
+          log = batch.log ++ log
+        )
+
       case Milling(log, started) =>
-        batch = batch.copy(process = batch.process.copy(millingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(millingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Milled(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(millingCompleted = completed),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(millingCompleted = completed),
+          log = batch.log ++ log
+        )
+
       case Mashing(log, started) =>
-        batch = batch.copy(process = batch.process.copy(mashingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(mashingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Mashed(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(mashingCompleted = completed),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(mashingCompleted = completed),
+          log = batch.log ++ log
+        )
+
       case MashingTempPhLogged(mashingTemp, pH, log) =>
-        batch = batch.copy(mashingTemp = mashingTemp,
+        batch = batch.copy(
+          mashingTemp = mashingTemp,
           pH = pH,
-          log = batch.log ++ log)
+          log = batch.log ++ log
+        )
+
       case Lautering(log, started) =>
-        batch = batch.copy(process = batch.process.copy(lauteringStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(lauteringStarted = started),
+          log = batch.log ++ log
+        )
+
       case Lautered(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(lauteringCompleted = completed),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(lauteringCompleted = completed),
+          log = batch.log ++ log
+        )
+
       case Sparging(log, started) =>
-        batch = batch.copy(process = batch.process.copy(spargingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(spargingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Sparged(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(spargingCompleted = completed),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(spargingCompleted = completed),
+          log = batch.log ++ log
+        )
+
       case MashEfficiencyLogged(mashEfficiency, log) =>
-        batch = batch.copy(mashEfficiency = mashEfficiency,
-          log = batch.log ++ log)
+        batch = batch.copy(
+          mashEfficiency = mashEfficiency,
+          log = batch.log ++ log
+        )
+
       case Boiling(log, started) =>
-        batch = batch.copy(process = batch.process.copy(boilingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(boilingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Boiled(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(boilingCompleted = completed),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(boilingCompleted = completed),
+          log = batch.log ++ log
+        )
+
       case Cooling(log, started) =>
-        batch = batch.copy(process = batch.process.copy(coolingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(coolingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Cooled(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(coolingCompleted = completed),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(coolingCompleted = completed),
+          log = batch.log ++ log
+        )
+
       case Whirlpooling(log, started) =>
-        batch = batch.copy(process = batch.process.copy(whirlpoolingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(whirlpoolingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Whirlpooled(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(whirlpoolingCompleted = completed),
-          log = batch.log ++ log)
-      case BoilingCoolingTempOriginalGravityLogged(boilingTemp,
-      coolingTemp,
-      originalGravity,
-      log) =>
-        batch = batch.copy(boilingTemp = boilingTemp,
+        batch = batch.copy(
+          process = batch.process.copy(whirlpoolingCompleted = completed),
+          log = batch.log ++ log
+        )
+
+      case BoilingCoolingTempOriginalGravityLogged(boilingTemp, coolingTemp, originalGravity, log) =>
+        batch = batch.copy(
+          boilingTemp = boilingTemp,
           coolingTemp = coolingTemp,
           originalGravity = originalGravity,
-          log = batch.log ++ log)
+          log = batch.log ++ log
+        )
+
       case Fermenting(log, started) =>
-        batch = batch.copy(process = batch.process.copy(fermentingStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(fermentingStarted = started),
+          log = batch.log ++ log
+        )
+
       case Fermented(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(fermentingCompleted = completed),
-          log = batch.log ++ log)
-      case FermentingTempFinalGravityLogged(fermentingTemp,
-      finalGravity,
-      log) =>
-        batch = batch.copy(fermentingTemp = fermentingTemp,
+        batch = batch.copy(
+          process = batch.process.copy(fermentingCompleted = completed),
+          log = batch.log ++ log
+        )
+
+      case FermentingTempFinalGravityLogged(fermentingTemp, finalGravity, log) =>
+        batch = batch.copy(
+          fermentingTemp = fermentingTemp,
           finalGravity = finalGravity,
-          log = batch.log ++ log)
+          log = batch.log ++ log
+        )
+
       case Conditioning(log, started) =>
-        batch = batch.copy(process = batch.process.copy(conditioningStarted = started),
-          log = batch.log ++ log)
+        batch = batch.copy(
+          process = batch.process.copy(conditioningStarted = started),
+          log = batch.log ++ log
+        )
+
       case Conditioned(log, completed) =>
-        batch = batch.copy(process = batch.process.copy(conditioningCompleted = completed),
-          log = batch.log ++ log)
-      case ConditioningTempSrmColorLogged(conditioningTemp,
-      srmColor,
-      log) =>
-        batch = batch.copy(conditioningTemp = conditioningTemp,
+        batch = batch.copy(
+          process = batch.process.copy(conditioningCompleted = completed),
+          log = batch.log ++ log
+        )
+
+      case ConditioningTempSrmColorLogged(conditioningTemp, srmColor, log) =>
+        batch = batch.copy(
+          conditioningTemp = conditioningTemp,
           srmColor = srmColor,
-          log = batch.log ++ log)
+          log = batch.log ++ log
+        )
+
       case Kegging(log, started) =>
-        batch = batch.copy(process = batch.process.copy(keggingStarted = started),
-          log = batch.log ++ log)
-      case Kegged(ibuBitterness,
-      alcoholByVolume,
-      alcoholByWeight,
-      calories,
-      keggingTemp,
-      appearance,
-      aroma,
-      taste,
-      log,
-      completed) =>
-        batch = batch.copy(ibuBitterness = ibuBitterness,
+        batch = batch.copy(
+          process = batch.process.copy(keggingStarted = started),
+          log = batch.log ++ log
+        )
+
+      case Kegged(ibuBitterness, alcoholByVolume, alcoholByWeight, calories, keggingTemp, appearance, aroma, taste, log, completed) =>
+        batch = batch.copy(
+          ibuBitterness = ibuBitterness,
           alcoholByVolume = alcoholByVolume,
           alcoholByWeight = alcoholByWeight,
           calories = calories,
@@ -156,13 +232,17 @@ final class Listener:
           appearance = appearance,
           aroma = aroma,
           taste = taste,
-          log = batch.log ++ log)
-        batch = batch.copy(process = batch.process.copy(keggingCompleted = completed))
-      case KeggingTempBrewhouseEfficiencyLogged(brewhouseEfficiency,
-      log,
-      completed) =>
-        batch = batch.copy(brewhouseEfficiency = brewhouseEfficiency,
+          log = batch.log ++ log
+        )
+        batch = batch.copy(
+          process = batch.process.copy(keggingCompleted = completed)
+        )
+
+      case KeggingTempBrewhouseEfficiencyLogged(brewhouseEfficiency, log, completed) =>
+        batch = batch.copy(
+          brewhouseEfficiency = brewhouseEfficiency,
           log = batch.log ++ log,
-          completed = completed)
+          completed = completed
+        )
     }
     batch
