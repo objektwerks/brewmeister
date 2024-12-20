1e9370a6cba87d8c7f5ced1c60e81ab33d1b58e6
objektwerks <objektwerks@runbox.com>
2024-12-20 15:53:32 -0500

    enhanced log dialog

--- src/main/scala/objektwerks/ui/dialog/LogDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/LogDialog.scala
@@ -7,3 +7,4 @@ import objektwerks.ui.{App, Context}
 final class LogDialog(context: Context) extends Dialog[Unit]:
   initOwner(App.stage)
   title = context.windowTitle
+  headerText = context.dialogBrew
