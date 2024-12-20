a0f2a12d61639494e8d38d9bc20e4b9000de0d38
objektwerks <objektwerks@runbox.com>
2024-12-20 15:51:39 -0500

    added log dialog

---
+++ src/main/scala/objektwerks/ui/dialog/LogDialog.scala
@@ -0,0 +1,9 @@
+package objektwerks.ui.dialog
+
+import scalafx.scene.control.Dialog
+
+import objektwerks.ui.{App, Context}
+
+final class LogDialog(context: Context) extends Dialog[Unit]:
+  initOwner(App.stage)
+  title = context.windowTitle
