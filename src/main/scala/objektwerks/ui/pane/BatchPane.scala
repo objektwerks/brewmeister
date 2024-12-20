41eda8a3254ff28e6f795e2db7b9483f3d935087
objektwerks <objektwerks@runbox.com>
2024-12-20 13:09:00 -0500

    enhanced batch pane

--- src/main/scala/objektwerks/ui/pane/BatchPane.scala
+++ src/main/scala/objektwerks/ui/pane/BatchPane.scala
@@ -1,5 +1,6 @@
 package objektwerks.ui.pane
 
+import scalafx.scene.control.Label
 import scalafx.scene.layout.{Priority, VBox}
 
 import objektwerks.ui.{Context, Model}
@@ -11,5 +12,9 @@ final class BatchPane(context: Context, model: Model) extends VBox:
   model.selectedBatch.onChange { (_, _, selectedBatch) =>
     // TODO - bind selected batch to this pane!
   }
+
+  // Controls
+  val labelRecipe = Label( context.labelRecipe )
+  val textRecipe = Label( model.selectedBatch.value.recipe )
 
   VBox.setVgrow(this, Priority.Always)
