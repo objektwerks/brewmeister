a5ae180938da448f0b4ea72f15e2f11c3edbfcb0
objektwerks <objektwerks@runbox.com>
2024-12-28 10:35:46 -0500

    enhanced remove confirmation dialog

--- src/main/scala/objektwerks/ui/dialog/RemoveConfirmationDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/RemoveConfirmationDialog.scala
@@ -7,3 +7,6 @@ import objektwerks.ui.App
 
 final class RemoveConfirmationDialog() extends Alert(AlertType.Confirmation):
   initOwner(App.stage)
+  title = "Remove Confirmation Dialog"
+  headerText = "Confirm Remove"
+  contentText = "Select Ok to confirm, Cancel to cancel."
