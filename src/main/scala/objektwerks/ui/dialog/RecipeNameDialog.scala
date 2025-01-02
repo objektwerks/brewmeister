3a78d53808c2cee5030378e7c11256c43d88ac4b
objektwerks <objektwerks@runbox.com>
2025-01-02 12:03:34 -0500

    enhanced recipe name dialog

--- src/main/scala/objektwerks/ui/dialog/RecipeNameDialog.scala
+++ src/main/scala/objektwerks/ui/dialog/RecipeNameDialog.scala
@@ -2,4 +2,11 @@ package objektwerks.ui.dialog
 
 import scalafx.scene.control.TextInputDialog
 
-final class RecipeNameDialog extends TextInputDialog
+import objektwerks.ui.{App, Context}
+
+final class RecipeNameDialog(context: Context,
+                             defaultValue: String) extends TextInputDialog(defaultValue):
+  initOwner(App.stage)
+  title = "Text Input Dialog"
+  headerText = "Look, a Text Input Dialog."
+  contentText = "Please enter your name:"
