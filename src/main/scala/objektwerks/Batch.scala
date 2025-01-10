0f048ab5a03272c1c13a050a7d66ab615c819cdb
objektwerks <objektwerks@runbox.com>
2025-01-10 14:04:41 -0500

    refactored batch brewhouse efficiency def

--- src/main/scala/objektwerks/Batch.scala
+++ src/main/scala/objektwerks/Batch.scala
@@ -52,7 +52,9 @@ object Batch:
     ( (actualFermentableExtract / potentialFermentableExtract) * 100 ).toInt
 
   def brewhouseEfficiency(batches: List[Batch]): Int =
-    ( batches.map(batch => batch.brewhouseEfficiency).sum / batches.length ).toInt
+    if batches.nonEmpty then
+      ( batches.map(batch => batch.brewhouseEfficiency).sum / batches.length ).toInt
+    else 0
 
 @upickle.implicits.serializeDefaults(true)
 final case class Batch(recipe: String = "",
