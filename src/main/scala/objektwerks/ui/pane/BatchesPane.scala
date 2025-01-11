c996f20d93aa3a7bbc8f1e2fb76d6f852bc75fa9
objektwerks <objektwerks@runbox.com>
2025-01-11 06:26:30 -0500

    enhanced batches pane remove def

--- src/main/scala/objektwerks/ui/pane/BatchesPane.scala
+++ src/main/scala/objektwerks/ui/pane/BatchesPane.scala
@@ -67,9 +67,9 @@ final class BatchesPane(context: Context, model: Model) extends TabPane:
   def remove(): Unit =
     RemoveConfirmationDialog(context).showAndWait() match
       case Some(ButtonType.OK) =>
+        val index = tableView.selectionModel().selectedIndex.value
         val batch = tableView.selectionModel().selectedItemProperty().value
-        if model.remove(batch) then
-          val index = tableView.selectionModel().selectedIndex.value
+        if model.remove(batch) && index > -1 then
           tableView.items.value.remove(index)
           buttonRemove.disable = true
       case _ =>
