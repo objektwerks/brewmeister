e8386b4ad9e1d9fe3c2f8293cf59b7d35d1dc410
objektwerks <objektwerks@runbox.com>
2025-01-17 20:29:16 -0500

    adde brew

--- src/main/scala/objektwerks/Batch.scala
+++ src/main/scala/objektwerks/Batch.scala
@@ -1,63 +1,11 @@
 package objektwerks
 
-import scala.math.pow
-
 import scalafx.beans.property.ObjectProperty
 
 import upickle.default.{ReadWriter => JsonSupport}
 
-object Batch:
+object Batch extends Brew:
   given Ordering[Batch] = Ordering.by[Batch, String](b => b.completed).reverse
-
-  def srmColor(batchVolume: Volume, grains: List[Grain]): Int =
-    val sum = grains.map { grain => srmColor(grain.weight, grain.color, batchVolume.value) }.sum
-    sum / grains.length
-
-  def srmColor(grainWeight: Double,
-               grainColor: Double,
-               batchVolume: Double): Int =
-    val maltColorUnits = (grainWeight * grainColor) / batchVolume
-    ( 1.4922 * pow(maltColorUnits, 0.6859) ).toInt
-
-  def ibuBitterness(hops: List[Hop]): Int =
-    hops.map { hop => ibuBitterness(hop.alphaAcid, hop.weight, hop.volume) }.sum
-
-  def ibuBitterness(hopAlphaAcid: Double,
-                    hopWeight: Double,
-                    hopVolume: Double): Int =
-    val hopUtilization = (hopAlphaAcid * hopWeight) / hopVolume
-    ( (hopWeight * hopAlphaAcid * hopUtilization) / 7.25 ).toInt
-
-  def alcoholByVolume(originalGravity: Double,
-                      finalGravity: Double): Double =
-    ( (originalGravity - finalGravity) * 131 ).format
-
-  def alcoholByWeight(alcoholByVolume: Double,
-                      finalGravity: Double): Double =
-    ( (0.79 * alcoholByVolume) / finalGravity ).format
-
-  def calories(packageVolume: Double,
-               originalGravity: Double,
-               finalGravity: Double): Int =
-    val alcoholCalories = (originalGravity - finalGravity) * 7.5
-    val carbohydrateCalories = (finalGravity * 13) * packageVolume
-    ( alcoholCalories + carbohydrateCalories ).toInt
-
-  def mashEfficiency(actualMashExtract: Double,
-                     potentialMashExtract: Double): Int =
-    val result = ( (actualMashExtract / potentialMashExtract) * 100 ).toInt
-    if result > 100 then 0 else result
-
-  def brewhouseEfficiency(actualFermentableExtract: Double,
-                          potentialFermentableExtract: Double): Int =
-    val result = ( (actualFermentableExtract / potentialFermentableExtract) * 100 ).toInt
-    if result > 100 then 0 else result
-
-  def brewhouseEfficiency(batches: List[Batch]): Int =
-    if batches.nonEmpty then
-      val result = ( batches.map(batch => batch.brewhouseEfficiency).sum / batches.length ).toInt
-      if result > 100 then 0 else result
-    else 0
 
 @upickle.implicits.serializeDefaults(true)
 final case class Batch(recipe: String = "",
