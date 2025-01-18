00b70351b030daf0bed4aa8b443a6def69718a21
objektwerks <objektwerks@runbox.com>
2025-01-17 20:30:22 -0500

    renamed test

--- src/test/scala/objektwerks/BrewTest.scala
+++ src/test/scala/objektwerks/BrewTest.scala
@@ -3,7 +3,7 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class BrewhMathTest extends AnyFunSuite with Matchers:
+class BrewTest extends AnyFunSuite with Matchers:
   test("srm color")
 
   test("ibu bitterness")
