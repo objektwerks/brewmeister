380844594c21230e3c39331d0ab23cca3182b9a8
objektwerks <objektwerks@runbox.com>
2025-02-04 07:08:23 -0500

    added onchange

--- src/main/scala/objektwerks/ui/pane/RecipePane.scala
+++ src/main/scala/objektwerks/ui/pane/RecipePane.scala
@@ -354,7 +354,9 @@ final class RecipePane(context: Context, model: Model) extends VBox:
     setShowTickMarks(true)
     setShowTickLabels(true)
     setBlockIncrement(1.0)
-
+    lowValueProperty.onChange { (_, _, _) => enableSave() }
+    highValueProperty.onChange { (_, _, _) => enableSave() }
+a
   val labelCalorieRange = Label( context.labelCalorieRange )
   val rangeSliderCalories = new RangeSlider(50, 250, model.selectedRecipe.value.calorieRange.low, model.selectedRecipe.value.calorieRange.high):
     setShowTickMarks(true)
