f3d1b6193d8d4ad21b4947ae5826b25c67e8f910
objektwerks <objektwerks@runbox.com>
2025-02-04 11:07:10 -0500

    enhanced label range slider

--- src/main/scala/objektwerks/ui/control/LabelRangeSlider.scala
+++ src/main/scala/objektwerks/ui/control/LabelRangeSlider.scala
@@ -2,17 +2,20 @@ package objektwerks.ui.control
 
 import org.controlsfx.control.RangeSlider
 
+import scalafx.Includes.*
 import scalafx.scene.control.Label
 import scalafx.scene.layout.HBox
 
-class LabelRangeSlider(min: Double,
-                       max: Double,
-                       changingMin: Double,
-                       changingMax: Double) extends HBox:
+final class LabelRangeSlider(min: Double,
+                             max: Double,
+                             changingMin: Double,
+                             changingMax: Double) extends HBox:
   val slider = new RangeSlider(min, max, changingMin, changingMax):
     setShowTickMarks(true)
     setShowTickLabels(true)
     setBlockIncrement(1.0)
+    lowValueProperty.onChange { (_, _, newValue) => labelChangingMin.text = newValue.toString  }
+    highValueProperty.onChange { (_, _, newValue) => labelChangingMax.text = newValue.toString  }
 
   val labelChangingMin = new Label():
     prefWidth = 50
