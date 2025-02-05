63c2246afcd9d42508502bf7a366595f301103fa
objektwerks <objektwerks@runbox.com>
2025-02-05 10:43:56 -0500

    added view bounds onchange to scrollpane

--- src/main/scala/objektwerks/ui/pane/BatchPane.scala
+++ src/main/scala/objektwerks/ui/pane/BatchPane.scala
@@ -165,7 +165,10 @@ final class BatchPane(context: Context, model: Model) extends VBox:
 
   val scrollPaneControls = new ScrollPane:
     content = ControlGrid(controls)
-    fitToWidth = true
+    viewportBounds.onChange { (_, _, bounds) =>
+      fitToWidth = content.value.prefWidth(-1) < bounds.getWidth
+      fitToHeight = content.value.prefHeight(-1) < bounds.getHeight
+    }
 
   // Buttons  
   val buttonLog = new Button:
