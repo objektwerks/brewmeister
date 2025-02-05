237d7d17a444f861b8fb1030529395a947ae9780
objektwerks <objektwerks@runbox.com>
2025-02-05 08:39:33 -0500

    enhanced menu

--- src/main/scala/objektwerks/ui/Menu.scala
+++ src/main/scala/objektwerks/ui/Menu.scala
@@ -3,12 +3,19 @@ package objektwerks.ui
 import scalafx.Includes._
 import scalafx.application.Platform
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{Menu => MenuRoot, MenuBar, MenuItem, SeparatorMenuItem}
+import scalafx.scene.control.{Alert, Menu => MenuRoot, MenuBar, MenuItem, SeparatorMenuItem}
+import scalafx.scene.control.Alert.AlertType
 
 final class Menu(context: Context) extends MenuBar:
   val menuItemAbout = new MenuItem:
     text = context.menuExit
-    onAction = (_: ActionEvent) => Platform.exit()
+    onAction = (_: ActionEvent) =>
+      new Alert(AlertType.Information) {
+        initOwner(App.stage)
+        title = context.windowTitle
+        headerText = "Brewmeister"
+        contentText = "Version 1.0.13, Apache 2.0 License"
+      }.showAndWait()
 
   val menuItemExit = new MenuItem:
     text = context.menuExit
