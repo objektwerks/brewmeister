d1f39561ce861cc55bf36cfe54986fad8b70ed73
objektwerks <objektwerks@runbox.com>
2025-02-05 09:42:52 -0500

    edited control grid

--- src/main/scala/objektwerks/ui/control/ControlGrid.scala
+++ src/main/scala/objektwerks/ui/control/ControlGrid.scala
@@ -9,11 +9,12 @@ final class ControlGrid(controls: List[(Label, Node)]) extends GridPane:
   hgap = 6
   vgap = 6
   padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
-  GridPane.setHgrow(this, Priority.Always)
-  GridPane.setVgrow(this, Priority.Always)
 
   var row = 0
   for ((label, node) <- controls)
     add(label, columnIndex = 0, rowIndex = row)
     add(node, columnIndex = 1, rowIndex = row)
     row += 1
+
+  GridPane.setHgrow(this, Priority.Always)
+  GridPane.setVgrow(this, Priority.Always)
