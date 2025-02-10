7e41c7e890740a559a836a66c47c1a117355f74c
objektwerks <objektwerks@runbox.com>
2025-02-10 11:35:08 -0500

    enhanced log pane

--- src/main/scala/objektwerks/ui/pane/LogPane.scala
+++ src/main/scala/objektwerks/ui/pane/LogPane.scala
@@ -3,5 +3,12 @@ package objektwerks.ui.pane
 import scalafx.geometry.Insets
 import scalafx.scene.layout.VBox
 
-final class LogPane extends VBox:
+import objektwerks.ui.{Context, Model}
+
+final class LogPane(context: Context, model: Model) extends VBox:
   padding = Insets(3)
+
+  // Model
+  model.selectedBatch.onChange { (_, _, selectedBatch) =>
+
+  }
