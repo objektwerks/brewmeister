6497e0ec13c668044db3d86e93776fa4e018c1c7
objektwerks <objektwerks@runbox.com>
2025-02-10 11:38:22 -0500

    enhanced log pane

--- src/main/scala/objektwerks/ui/pane/LogPane.scala
+++ src/main/scala/objektwerks/ui/pane/LogPane.scala
@@ -1,6 +1,7 @@
 package objektwerks.ui.pane
 
 import scalafx.geometry.Insets
+import scalafx.scene.control.{Label, ListView}
 import scalafx.scene.layout.VBox
 
 import objektwerks.ui.{Context, Model}
@@ -12,3 +13,6 @@ final class LogPane(context: Context, model: Model) extends VBox:
   model.selectedBatch.onChange { (_, _, selectedBatch) =>
 
   }
+
+  val labelLog = Label( context.dialogLog )
+  val listViewLog = ListView( model.selectedBatch.value.log )
