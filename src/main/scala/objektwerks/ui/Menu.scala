4bce2e6aa8821e15886c754df52960913e2cc8da
objektwerks <objektwerks@runbox.com>
2025-02-13 11:04:32 -0500

    added comment

--- src/main/scala/objektwerks/ui/Menu.scala
+++ src/main/scala/objektwerks/ui/Menu.scala
@@ -6,6 +6,19 @@ import scalafx.event.ActionEvent
 import scalafx.scene.control.{Alert, Menu => MenuRoot, MenuBar, MenuItem, SeparatorMenuItem}
 import scalafx.scene.control.Alert.AlertType
 
+/*
+TextArea textArea = new TextArea("YOUR_MESSAGE_HERE");
+textArea.setEditable(false);
+textArea.setWrapText(true);
+GridPane gridPane = new GridPane();
+gridPane.setMaxWidth(Double.MAX_VALUE);
+gridPane.add(textArea, 0, 0);
+
+Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+alert.setTitle("Stuff");
+alert.getDialogPane().setContent(gridPane);
+alert.showAndWait();
+*/
 final class Menu(context: Context) extends MenuBar:
   val menuItemAbout = new MenuItem:
     text = context.menuAbout
